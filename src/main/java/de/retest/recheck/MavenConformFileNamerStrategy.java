6369e9d9ac65b7084ef0b84f6cd5987185b056c0
Daniel Kraus <daniel.kraus@retest.de>
2018-09-24 15:12:06 +0200

    Refactor constants

--- src/main/java/de/retest/recheck/MavenConformFileNamerStrategy.java
+++ src/main/java/de/retest/recheck/MavenConformFileNamerStrategy.java
@@ -8,7 +8,8 @@ import de.retest.persistence.FileNamer;
 
 public class MavenConformFileNamerStrategy implements FileNamerStrategy {
 
-	public static final String WORKSPACE_DEFAULT = "src/test/resources/retest/";
+	public static final String DEFAULT_RETEST_WORKSPACE_PATH = "src/test/resources/retest/";
+	public static final String DEFAULT_RETEST_TESTREPORTS_PATH = "target/test-classes/retest/";
 
 	@Override
 	public FileNamer createFileNamer( final String... baseNames ) {
@@ -17,15 +18,15 @@ public class MavenConformFileNamerStrategy implements FileNamerStrategy {
 			@Override
 			public File getFile( final String extension ) {
 				final String baseName = Joiner.on( "/" ).join( baseNames );
-				return new File( WORKSPACE_DEFAULT + File.separator + RECHECK_FOLDER_NAME + File.separator + baseName
-						+ extension );
+				return new File( DEFAULT_RETEST_WORKSPACE_PATH + File.separator + RECHECK_FOLDER_NAME + File.separator
+						+ baseName + extension );
 			}
 
 			@Override
 			public File getResultFile( final String extension ) {
 				final String baseName = Joiner.on( "/" ).join( baseNames );
-				return new File( "target/test-classes/retest/" + File.separator + RECHECK_FOLDER_NAME
-						+ File.separator + baseName + extension );
+				return new File( DEFAULT_RETEST_TESTREPORTS_PATH + File.separator + RECHECK_FOLDER_NAME + File.separator
+						+ baseName + extension );
 			}
 		};
 	}
