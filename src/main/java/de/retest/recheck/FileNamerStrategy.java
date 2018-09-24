288cea4cfc1c4ab406643910b5ac147661d037a8
Jeremias Roessler <roessler@retest.de>
2018-09-24 13:40:24 +0200

    Extract String contents for recheck into constants

--- src/main/java/de/retest/recheck/FileNamerStrategy.java
+++ src/main/java/de/retest/recheck/FileNamerStrategy.java
@@ -4,6 +4,10 @@ import de.retest.persistence.FileNamer;
 
 public interface FileNamerStrategy {
 
+	public static final String RECHECK_FILE_EXTENSION = "recheck";
+	public static final String RECHECK_FOLDER_PROPERTY = "de.retest.RecheckFolder";
+	public static final String RECHECK_FOLDER_DEFAULT = "recheck";
+
 	public FileNamer createFileNamer( String... baseNames );
 
 	public String getTestClassName();
