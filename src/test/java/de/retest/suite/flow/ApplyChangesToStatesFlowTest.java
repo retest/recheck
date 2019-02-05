ac5cf8b2e8b9a055fbb8028868ba65df9552fc96
Marius Kemmling <marius.kemmling@retest.de>
2019-02-05 10:56:17 +0100

    Improve test

--- src/test/java/de/retest/suite/flow/ApplyChangesToStatesFlowTest.java
+++ src/test/java/de/retest/suite/flow/ApplyChangesToStatesFlowTest.java
@@ -1,7 +1,5 @@
 package de.retest.suite.flow;
 
-
-import static de.retest.Properties.WORK_DIRECTORY;
 import static de.retest.persistence.RecheckStateFileProviderImpl.RECHECK_PROJECT_ROOT;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,13 +7,12 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.rules.TemporaryFolder;
 
-import de.retest.Properties;
 import de.retest.configuration.Configuration;
 import de.retest.persistence.Persistence;
 import de.retest.persistence.PersistenceFactory;
@@ -29,47 +26,45 @@ import de.retest.util.junit.vintage.SystemProperty;
 
 public class ApplyChangesToStatesFlowTest {
 
-	private static final Logger logger = LoggerFactory.getLogger( ApplyChangesToStatesFlowTest.class );
-
 	private static final String SUITE = "ApplyChangesToStatesFlowTest";
 	private static final String TEST = "check_GUI_with_review_license";
 	private static final String ACTION = "launch";
-	private static final String FILE = "target/test-classes/recheck/" + SUITE + "/" + TEST + "." + ACTION + ".recheck";
+	private static final String FILE = "src/test/resources/recheck/" + SUITE + "/" + TEST + "." + ACTION + ".recheck";
 
 	@Rule
-	public SystemProperty workDirProp = new SystemProperty( WORK_DIRECTORY );
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
 	@Rule
 	public SystemProperty recheckDirProp = new SystemProperty( RECHECK_PROJECT_ROOT );
 
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		Configuration.resetRetest();
-		workDirProp.setValue( "target/test-classes" );
-		logger.info( "Workspace now pointing to {}.", Configuration.getRetestWorkspace() );
-		recheckDirProp.setValue( Properties.getReTestInstallDir().toString() );
+		recheckDirProp.setValue( "/" );
 	}
 
 	@Test
 	public void apply_should_apply_changes_to_states() throws Exception {
 		final PersistenceFactory persistenceFactory =
 				new PersistenceFactory( new HashSet<>( Arrays.asList( StdXmlClassesProvider.getXmlDataClasses() ) ) );
+
 		final Persistence<SutState> persistence = persistenceFactory.getPersistence();
-		final File file = new File( FILE );
-		final SutState before = persistence.load( file.toURI() );
-		logger.debug( "Before loaded" );
+		final File originalFile = new File( FILE );
+		final File temporaryFile = temporaryFolder.newFile("check_GUI_with_review_license.launch.recheck");
+
+		FileUtils.copyFile( originalFile, temporaryFile );
+		final SutState before = persistence.load( temporaryFile.toURI() );
 
 		final ReviewResult reviewResult = new ReviewResult();
 		final SuiteChangeSet acceptedChanges = reviewResult.createSuiteChangeSet( SUITE, "" );
 		final Element toChange = before.getRootElements().get( 0 ).getContainedElements().get( 0 )
 				.getContainedElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements().get( 0 )
 				.getContainedElements().get( 0 );
-		acceptedChanges.createTestChangeSet().createActionChangeSet( ACTION, file.getPath() ).getAttributesChanges()
+		acceptedChanges.createTestChangeSet().createActionChangeSet( ACTION, temporaryFile.getPath() ).getAttributesChanges()
 				.add( toChange.getIdentifyingAttributes(), new AttributeDifference( "enabled", "false", "true" ) );
-		logger.debug( "AcceptedChanges created." );
 
 		ApplyChangesToStatesFlow.apply( persistence, acceptedChanges );
-		final SutState after = persistence.load( file.toURI() );
+		final SutState after = persistence.load( temporaryFile.toURI() );
 		final Element changed = after.getRootElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements()
 				.get( 0 ).getContainedElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements()
 				.get( 0 );
