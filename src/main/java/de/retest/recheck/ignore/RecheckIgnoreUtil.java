cd85f4189f3202c58ed830b687ae08ffbb3ff492
Johannes Dillmann <johannes.dillmann@retest.de>
2019-02-18 13:30:47 +0100

    Add helper to find project ignore file

---
+++ src/main/java/de/retest/recheck/ignore/RecheckIgnoreUtil.java
@@ -0,0 +1,26 @@
+package de.retest.recheck.ignore;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+import de.retest.recheck.configuration.ProjectConfigurationUtil;
+
+public class RecheckIgnoreUtil {
+
+	public static final String RECHECK_IGNORE = "recheck.ignore";
+
+	private RecheckIgnoreUtil() {
+
+	}
+
+	public static Optional<Path> getIgnoreFile() {
+		final Path projectRoot = ProjectConfigurationUtil.findProjectConfiguration();
+		final Path ignoreFile = projectRoot.resolve( RECHECK_IGNORE );
+
+		if ( ignoreFile.toFile().exists() ) {
+			return Optional.of( ignoreFile );
+		}
+
+		return Optional.empty();
+	}
+}
