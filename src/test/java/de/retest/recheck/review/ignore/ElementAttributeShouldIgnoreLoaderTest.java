2a7204695c34f268119a385e69b4759cf2e2f9a2
Johannes Dillmann <johannes.dillmann@retest.de>
2019-02-25 11:55:56 +0100

    Use retest id as defaullt

--- src/test/java/de/retest/recheck/review/ignore/ElementAttributeShouldIgnoreLoaderTest.java
+++ src/test/java/de/retest/recheck/review/ignore/ElementAttributeShouldIgnoreLoaderTest.java
@@ -1,16 +1,15 @@
 package de.retest.recheck.review.ignore;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import de.retest.recheck.ui.descriptors.Element;
 import de.retest.recheck.review.ignore.ElementAttributeShouldIgnore.ElementAttributeShouldIgnoreLoader;
-import de.retest.recheck.review.ignore.matcher.ElementIdMatcher;
+import de.retest.recheck.review.ignore.matcher.ElementRetestIdMatcher;
+import de.retest.recheck.ui.descriptors.Element;
 
 class ElementAttributeShouldIgnoreLoaderTest {
 
@@ -23,17 +22,17 @@ class ElementAttributeShouldIgnoreLoaderTest {
 
 		final Element element = mock( Element.class );
 		when( element.getRetestId() ).thenReturn( "abc" );
-		ignore = new ElementAttributeShouldIgnore( new ElementIdMatcher( element ), "123" );
+		ignore = new ElementAttributeShouldIgnore( new ElementRetestIdMatcher( element ), "123" );
 	}
 
 	@Test
 	void save_should_produce_correct_line() {
-		assertThat( cut.save( ignore ) ).isEqualTo( "matcher: id=abc, key: 123" );
+		assertThat( cut.save( ignore ) ).isEqualTo( "matcher: retestid=abc, key: 123" );
 	}
 
 	@Test
 	void load_should_produce_correct_ignore() {
-		final String line = "matcher: id=abc, key: 123";
+		final String line = "matcher: retestid=abc, key: 123";
 		assertThat( cut.save( cut.load( line ) ) ).isEqualTo( line );
 	}
 }
