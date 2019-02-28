3da4eead1fb9604767b9e432b98db150c4f418b0
Daniel Kraus <daniel.kraus@retest.de>
2019-02-28 16:01:41 +0100

    Add test

---
+++ src/test/java/de/retest/recheck/MavenConformFileNamerStrategyTest.java
@@ -0,0 +1,25 @@
+package de.retest.recheck;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+
+import de.retest.recheck.persistence.FileNamer;
+
+class MavenConformFileNamerStrategyTest {
+
+	@Test
+	void files_should_be_maven_conform() throws Exception {
+		final FileNamerStrategy cut = new MavenConformFileNamerStrategy();
+
+		final FileNamer fileNamer = cut.createFileNamer( "foo", "bar" );
+		final File recheckFile = fileNamer.getFile( Properties.RECHECK_FILE_EXTENSION );
+		final File resultFile = fileNamer.getResultFile( Properties.REPORT_FILE_EXTENSION );
+
+		assertThat( recheckFile.getPath() ).isEqualTo( "src/test/resources/retest/recheck/foo/bar.recheck" );
+		assertThat( resultFile.getPath() ).isEqualTo( "target/test-classes/retest/recheck/foo/bar.result" );
+	}
+
+}
