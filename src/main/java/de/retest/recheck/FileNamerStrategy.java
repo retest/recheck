21b877d3a1338747ccaf027a453c5cea14309451
Daniel Kraus <daniel.kraus@retest.de>
2019-02-28 15:46:13 +0100

    Remove unnecessary access modifiers

--- src/main/java/de/retest/recheck/FileNamerStrategy.java
+++ src/main/java/de/retest/recheck/FileNamerStrategy.java
@@ -4,10 +4,10 @@ import de.retest.recheck.persistence.FileNamer;
 
 public interface FileNamerStrategy {
 
-	public static final String RECHECK_FILE_EXTENSION = ".recheck";
-	public static final String RECHECK_FOLDER_NAME = "recheck";
+	static final String RECHECK_FILE_EXTENSION = ".recheck";
+	static final String RECHECK_FOLDER_NAME = "recheck";
 
-	public FileNamer createFileNamer( String... baseNames );
+	FileNamer createFileNamer( String... baseNames );
 
 	default String getTestClassName() {
 		final StackTraceElement testMethodStack = TestCaseFinder.findTestCaseMethodInStack();
