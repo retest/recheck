ddb34de54d0c7e55357c1daf30a323badb0cd07d
narkaTee <narkaTee@users.noreply.github.com>
2019-03-03 13:58:52 +0100

    Add option to pass a custom sourceSetName

--- src/main/java/de/retest/recheck/GradleConformFileNamerStrategy.java
+++ src/main/java/de/retest/recheck/GradleConformFileNamerStrategy.java
@@ -1,13 +1,27 @@
 package de.retest.recheck;
 
 import java.io.File;
+import java.util.Objects;
 
 import de.retest.recheck.persistence.FileNamer;
 
 public class GradleConformFileNamerStrategy implements FileNamerStrategy {
+	private final String sourceSetName;
 
-	public static final String DEFAULT_RETEST_WORKSPACE_PATH = "src/test/resources/retest/";
-	public static final String DEFAULT_RETEST_TESTREPORTS_PATH = "build/test-results/test/";
+	public GradleConformFileNamerStrategy() {
+		this( "test" );
+	}
+
+	public GradleConformFileNamerStrategy( String sourceSetName ) {
+		Objects.requireNonNull( sourceSetName, "sourceSetName cannot be null!" );
+		if ( sourceSetName.isEmpty() ) {
+			throw new IllegalArgumentException( "sourceSetName cannot be empty!" );
+		}
+		this.sourceSetName = sourceSetName;
+	}
+
+	public static final String DEFAULT_RETEST_WORKSPACE_PATH = "src/%s/resources/retest/";
+	public static final String DEFAULT_RETEST_TESTREPORTS_PATH = "build/test-results/%s/";
 
 	@Override
 	public FileNamer createFileNamer( final String... baseNames ) {
@@ -24,8 +38,9 @@ public class GradleConformFileNamerStrategy implements FileNamerStrategy {
 		};
 	}
 
-	private File toFile( final String prefix, final String extension, final String... baseNames ) {
+	private File toFile( final String prefixFormat, final String extension, final String... baseNames ) {
 		final String baseName = String.join( File.separator, baseNames );
+		final String prefix = String.format( prefixFormat, sourceSetName );
 		return new File( prefix + File.separator + baseName + extension );
 	}
 
--- src/test/java/de/retest/recheck/GradleConformFileNamerStrategyTest.java
+++ src/test/java/de/retest/recheck/GradleConformFileNamerStrategyTest.java
@@ -1,6 +1,7 @@
 package de.retest.recheck;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.File;
 
@@ -11,7 +12,7 @@ import de.retest.recheck.persistence.FileNamer;
 class GradleConformFileNamerStrategyTest {
 
 	@Test
-	void files_should_be_maven_conform() throws Exception {
+	void files_should_be_gradle_conform() throws Exception {
 		final FileNamerStrategy cut = new GradleConformFileNamerStrategy();
 
 		final FileNamer fileNamer = cut.createFileNamer( "foo", "bar" );
@@ -22,4 +23,29 @@ class GradleConformFileNamerStrategyTest {
 		assertThat( resultFile.getPath() ).isEqualTo( "build/test-results/test/retest/recheck/foo/bar.result" );
 	}
 
+	@Test
+	void files_should_be_in_the_correct_source_set() {
+		final FileNamerStrategy cut = new GradleConformFileNamerStrategy( "integrationTest" );
+
+		final FileNamer fileNamer = cut.createFileNamer( "foo", "bar" );
+		final File recheckFile = fileNamer.getFile( Properties.RECHECK_FILE_EXTENSION );
+		final File resultFile = fileNamer.getResultFile( Properties.REPORT_FILE_EXTENSION );
+
+		assertThat( recheckFile.getPath() ).isEqualTo( "src/integrationTest/resources/retest/recheck/foo/bar.recheck" );
+		assertThat( resultFile.getPath() ).isEqualTo( "build/test-results/integrationTest/retest/recheck/foo/bar.result" );
+	}
+
+	@Test
+	void should_not_allow_null_source_set_name() {
+		assertThatThrownBy( () -> new GradleConformFileNamerStrategy( null ) )
+				.isExactlyInstanceOf( NullPointerException.class )
+				.hasMessage( "sourceSetName cannot be null!" );
+	}
+
+	@Test
+	void should_not_allow_empty_source_set_name() {
+		assertThatThrownBy( () -> new GradleConformFileNamerStrategy( "" ) )
+				.isExactlyInstanceOf( IllegalArgumentException.class )
+				.hasMessage( "sourceSetName cannot be empty!" );
+	}
 }
