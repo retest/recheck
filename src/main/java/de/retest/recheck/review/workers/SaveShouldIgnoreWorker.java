390fa2a3393025d4e8714f355d09fcbbd1b654b8
Daniel Kraus <daniel.kraus@retest.de>
2019-03-24 12:14:56 +0100

    Filter JSShouldIgnore

--- src/main/java/de/retest/recheck/review/workers/SaveShouldIgnoreWorker.java
+++ src/main/java/de/retest/recheck/review/workers/SaveShouldIgnoreWorker.java
@@ -5,9 +5,12 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import de.retest.recheck.ignore.JSShouldIgnoreImpl;
 import de.retest.recheck.ignore.RecheckIgnoreUtil;
+import de.retest.recheck.ignore.ShouldIgnore;
 import de.retest.recheck.review.GlobalIgnoreApplier;
 import de.retest.recheck.review.GlobalIgnoreApplier.PersistableGlobalIgnoreApplier;
 import de.retest.recheck.review.ignore.io.Loaders;
@@ -24,7 +27,10 @@ public class SaveShouldIgnoreWorker {
 		final Optional<Path> ignoreFile = RecheckIgnoreUtil.getIgnoreFile();
 		final PersistableGlobalIgnoreApplier persist = applier.persist();
 
-		final Stream<String> save = Loaders.save( persist.getIgnores().stream() );
+		// Filter JSShouldIgnore because that would create unnecessary file content.
+		final Stream<ShouldIgnore> ignores = persist.getIgnores().stream() //
+				.filter( ((Predicate<ShouldIgnore>) JSShouldIgnoreImpl.class::isInstance).negate() );
+		final Stream<String> save = Loaders.save( ignores );
 
 		try ( final PrintStream writer = new PrintStream( Files.newOutputStream(
 				ignoreFile.orElseThrow( () -> new IllegalArgumentException( "No recheck.ignore found." ) ) ) ) ) {
