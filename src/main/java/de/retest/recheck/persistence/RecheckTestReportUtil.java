cf2dcd7bdc2a7f3e1b39419a88df3d9b1d5565e1
Johannes Dillmann <johannes.dillmann@retest.de>
2019-03-25 15:51:11 +0100

    Use dynamic persistence factory

--- src/main/java/de/retest/recheck/persistence/RecheckTestReportUtil.java
+++ src/main/java/de/retest/recheck/persistence/RecheckTestReportUtil.java
@@ -1,5 +1,7 @@
 package de.retest.recheck.persistence;
 
+import static de.retest.recheck.XmlTransformerUtil.getXmlTransformer;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -7,7 +9,6 @@ import java.io.UncheckedIOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.retest.recheck.persistence.bin.KryoPersistence;
 import de.retest.recheck.report.SuiteReplayResult;
 import de.retest.recheck.report.TestReport;
 import de.retest.recheck.util.FileUtil;
@@ -16,15 +17,16 @@ public class RecheckTestReportUtil {
 
 	private static final Logger logger = LoggerFactory.getLogger( RecheckTestReportUtil.class );
 
-	private static final Persistence<TestReport> persistence = new KryoPersistence<>();
+	private static final PersistenceFactory persistenceFactory = new PersistenceFactory( getXmlTransformer() );
 
-	private RecheckTestReportUtil() {}
+	private RecheckTestReportUtil() {
+	}
 
 	public static void persist( final SuiteReplayResult suite, final File file ) {
 		logger.info( "Persisting test report to file '{}'.", FileUtil.canonicalPathQuietly( file ) );
 		try {
 			FileUtil.ensureFolder( file );
-			persistence.save( file.toURI(), TestReport.fromApi( suite ) );
+			persistenceFactory.getPersistence().save( file.toURI(), TestReport.fromApi( suite ) );
 		} catch ( final IOException e ) {
 			throw new UncheckedIOException( "Could not save test report.", e );
 		}
