c514c4a836861e50c9d4daeba9db611758658d39
Johannes Dillmann <johannes.dillmann@retest.de>
2019-03-27 17:20:53 +0100

    Extract as env variable

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -4,11 +4,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-###### Extract these ######
-export MVN="mvn --settings ${TRAVIS_BUILD_DIR}/.travis.settings.xml"
-
 ###### GPG ######
-
 if [ ! -f ${TRAVIS_BUILD_DIR}/already_signed ]; then
     # decrypt ReTest secret key
     openssl aes-256-cbc -K "${encrypted_58a7d4f10478_key}" -iv "${encrypted_58a7d4f10478_iv}" -in "${TRAVIS_BUILD_DIR}/retest-gmbh-gpg.asc.enc" -out "${TRAVIS_BUILD_DIR}/retest-gmbh-gpg.asc" -d
--- ci/script.sh
+++ ci/script.sh
@@ -4,9 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-###### Extract these ######
-export MVN="mvn --settings ${TRAVIS_BUILD_DIR}/.travis.settings.xml"
-
 ###### Maven ######
 if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
     ${MVN} clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
