aa04815182fcd8ad169abe4495623b83602512f4
Johannes Dillmann <johannes.dillmann@retest.de>
2019-03-27 17:20:32 +0100

    Run tests with java 11

--- ci/script.sh
+++ ci/script.sh
@@ -10,3 +10,8 @@ if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
 else
     ${MVN} clean org.jacoco:jacoco-maven-plugin:prepare-agent verify
 fi
+
+# Switch to JDK 11
+wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 11
+
+${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent verify
