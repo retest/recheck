15939834619221b8d704adb5a8d9c10bd042d872
Daniel Kraus <daniel.kraus@retest.de>
2019-04-04 12:35:06 +0200

    Adapt script to compile with 8 and test with 11

--- ci/script.sh
+++ ci/script.sh
@@ -5,13 +5,14 @@ set -o errexit
 set -o pipefail
 
 ###### Maven ######
-if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
-    ${MVN} clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
-else
-    ${MVN} clean org.jacoco:jacoco-maven-plugin:prepare-agent verify
-fi
+# Compile with JDK 8
+${MVN} clean test-compile
 
-# Switch to JDK 11
+# Test with JDK 11
 wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 11
 
-${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent verify
+if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
+    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
+else
+    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent verify
+fi
