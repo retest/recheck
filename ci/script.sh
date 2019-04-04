c04019611c5c8fa16d39a8bf4c99418e1e8fd91f
Daniel Kraus <daniel.kraus@retest.de>
2019-04-04 15:37:37 +0200

    Avoid Javadoc etc. by triggering integration tests directly

--- ci/script.sh
+++ ci/script.sh
@@ -11,8 +11,9 @@ ${MVN} clean package -DskipTests
 # Test with JDK 11
 wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 11
 
+# We cannot simply use verify due to https://bugs.openjdk.java.net/browse/JDK-8212233.
 if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
-    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
+    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent test failsafe:integration-test sonar:sonar
 else
-    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent verify
+    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent test failsafe:integration-test
 fi
