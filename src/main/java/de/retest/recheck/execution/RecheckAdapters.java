f65433781c92b85c70c7971d87f70acb69d29c12
Jeremias Roessler <roessler@retest.de>
2019-04-18 12:45:11 +0200

    Improve message if recheck instead of recheck-web was used

--- src/main/java/de/retest/recheck/execution/RecheckAdapters.java
+++ src/main/java/de/retest/recheck/execution/RecheckAdapters.java
@@ -9,16 +9,23 @@ public class RecheckAdapters {
 
 	private static final ServiceLoader<RecheckAdapter> adapters = ServiceLoader.load( RecheckAdapter.class );
 
-	private RecheckAdapters() {
-
-	}
+	private RecheckAdapters() {}
 
 	public static RecheckAdapter findAdapterFor( final Object toVerify ) {
 		return StreamSupport.stream( adapters.spliterator(), false ) //
 				.filter( adapter -> adapter.canCheck( toVerify ) ) //
 				.findAny() //
-				.orElseThrow( () -> new UnsupportedOperationException( String.format(
-						"No recheck adapter registered that can handle an object of %s.", toVerify.getClass() ) ) );
+				.orElseThrow( () -> createHelpfulExceptionForMissingAdapter( toVerify.getClass().getName() ) );
+	}
+
+	protected static UnsupportedOperationException createHelpfulExceptionForMissingAdapter( final String className ) {
+		final String msg =
+				String.format( "No recheck adapter registered that can handle an object of class %s.", className );
+		if ( className.startsWith( "org.openqa.selenium" ) ) {
+			return new UnsupportedOperationException(
+					msg + "\n You need to add recheck-web (https://github.com/retest/recheck-web) to the classpath." );
+		}
+		return new UnsupportedOperationException( msg );
 	}
 
 }
