4cc9693066ae29c2b8aeaf68b3ac8552595c096a
IgorAlbert <igor.albert@retest.de>
2019-04-23 15:39:39 +0200

    Search for customer filter Files

--- src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
+++ src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
@@ -1,12 +1,27 @@
 package de.retest.recheck.ignore;
 
+import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_CONFIG_FOLDER;
+
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import de.retest.recheck.configuration.ProjectRootFinderUtil;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SearchFilterFiles {
+
+	public static final String FILTER_FOLDER = "filter";
+	public static final Path PROJECT_ROOT = ProjectRootFinderUtil.getProjectRoot()
+			.orElseThrow( () -> new RuntimeException( "Project root could not be found." ) );
+	public static final Path PROJECT_CONFIG_FOLDER = PROJECT_ROOT.resolve( RETEST_PROJECT_CONFIG_FOLDER );
+	public static final Path COSTUMER_FILTER_FOLDER = PROJECT_CONFIG_FOLDER.resolve( FILTER_FOLDER );
 
 	private static final String BASIC_FILTER_DIR = "/filter/";
 	private static final String WEB_FILTER_DIR = BASIC_FILTER_DIR + "web/";
@@ -23,4 +38,15 @@ public class SearchFilterFiles {
 				.collect( Collectors.toList() ); //
 	}
 
+	public static List<File> getCostumerFilterFiles() {
+		try ( Stream<Path> paths = Files.walk( COSTUMER_FILTER_FOLDER ) ) {
+			return paths.filter( Files::isRegularFile )//
+					.filter( file -> file.toString().endsWith( ".filter" ) )//
+					.map( Path::toFile )//
+					.collect( Collectors.toList() );
+		} catch ( final Exception e ) {
+			log.info( "You can create your own filter files in the \\.retest\\filter folder" );
+		}
+		return null;
+	}
 }
