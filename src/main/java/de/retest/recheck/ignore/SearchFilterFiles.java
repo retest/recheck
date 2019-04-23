262022bbb7a4725d79536587909463f0175d5fe1
IgorAlbert <igor.albert@retest.de>
2019-04-23 11:57:30 +0200

    Change the log mechanism

--- src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
+++ src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
@@ -5,14 +5,11 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
-import de.retest.recheck.configuration.PathBasedProjectRootFinder;
-
+@Slf4j
 public class SearchFilterFiles {
 	private static final String PATH_FILTER_FILES = "filter/web/";
-	private static final Logger logger = LoggerFactory.getLogger( PathBasedProjectRootFinder.class );
 	private static final String[] DEFAULT_FILTER_FILES = { "positioning.filter", "visibility.filter" };
 
 	public List<File> getDefaultFilterFiles() {
@@ -20,8 +17,7 @@ public class SearchFilterFiles {
 		for ( final String filterName : DEFAULT_FILTER_FILES ) {
 			final URL resource = getClass().getClassLoader().getResource( PATH_FILTER_FILES + filterName );
 			if ( resource == null ) {
-				logger.debug( "The search for {} was unsuccessful, make sure that the file was not deleted.",
-						filterName );
+				log.debug( "The search for {} was unsuccessful, make sure that the file was not deleted.", filterName );
 			} else {
 				final File filterFile = new File( resource.getFile() );
 				solution.add( filterFile );
