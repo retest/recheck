6e2b8873f7d467da2ff23d1b1431fbd25106cd5c
IgorAlbert <igor.albert@retest.de>
2019-04-23 11:56:03 +0200

    Extract field for the filter path

--- src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
+++ src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
@@ -11,13 +11,14 @@ import org.slf4j.LoggerFactory;
 import de.retest.recheck.configuration.PathBasedProjectRootFinder;
 
 public class SearchFilterFiles {
+	private static final String PATH_FILTER_FILES = "filter/web/";
 	private static final Logger logger = LoggerFactory.getLogger( PathBasedProjectRootFinder.class );
 	private static final String[] DEFAULT_FILTER_FILES = { "positioning.filter", "visibility.filter" };
 
 	public List<File> getDefaultFilterFiles() {
 		final List<File> solution = new ArrayList<>();
 		for ( final String filterName : DEFAULT_FILTER_FILES ) {
-			final URL resource = getClass().getClassLoader().getResource( "filter/web/" + filterName );
+			final URL resource = getClass().getClassLoader().getResource( PATH_FILTER_FILES + filterName );
 			if ( resource == null ) {
 				logger.debug( "The search for {} was unsuccessful, make sure that the file was not deleted.",
 						filterName );
