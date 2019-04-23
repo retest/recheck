95e657d08b487cb2d573159588ab5fb39a117b2e
Daniel Kraus <daniel.kraus@retest.de>
2019-04-23 14:21:46 +0200

    Simplify impl

--- src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
+++ src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
@@ -1,33 +1,26 @@
 package de.retest.recheck.ignore;
 
 import java.io.File;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class SearchFilterFiles {
 
-	private static final String PATH_FILTER_FILES = "filter/web/";
-	private static final List<String> DEFAULT_FILTER_FILES = Arrays.asList( "positioning.filter", "visibility.filter" );
+	private static final String BASIC_FILTER_DIR = "/filter/";
+	private static final String WEB_FILTER_DIR = BASIC_FILTER_DIR + "web/";
+	private static final List<String> defaultWebFilter =
+			Arrays.asList( WEB_FILTER_DIR + "positioning.filter", WEB_FILTER_DIR + "visibility.filter" );
 
 	private SearchFilterFiles() {}
 
 	public static List<File> getDefaultFilterFiles() {
-		final List<File> solution = new ArrayList<>();
-		for ( final String filterName : DEFAULT_FILTER_FILES ) {
-			final URL resource = SearchFilterFiles.class.getClassLoader().getResource( PATH_FILTER_FILES + filterName );
-			if ( resource == null ) {
-				log.debug( "The search for {} was unsuccessful, make sure that the file was not deleted.", filterName );
-			} else {
-				final File filterFile = new File( resource.getFile() );
-				solution.add( filterFile );
-			}
-		}
-		return solution;
+		return defaultWebFilter.stream() //
+				.map( filter -> SearchFilterFiles.class.getResource( filter ) ) //
+				.filter( Objects::nonNull ) //
+				.map( resource -> new File( resource.getFile() ) ) //
+				.collect( Collectors.toList() ); //
 	}
 
 }
