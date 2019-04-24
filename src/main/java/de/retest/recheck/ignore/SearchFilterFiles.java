96b90c1135f378de965db2c265ca28e5c31d523e
IgorAlbert <igor.albert@retest.de>
2019-04-24 09:52:59 +0200

    Create a new method so the tests will work again

--- src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
+++ src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
@@ -1,10 +1,12 @@
 package de.retest.recheck.ignore;
 
+import static de.retest.recheck.configuration.ProjectConfiguration.FILTER_FOLDER;
 import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_CONFIG_FOLDER;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -17,18 +19,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SearchFilterFiles {
 
-	public static final String FILTER_FOLDER = "filter";
-	public static final Path PROJECT_ROOT = ProjectRootFinderUtil.getProjectRoot()
-			.orElseThrow( () -> new RuntimeException( "Project root could not be found." ) );
-	public static final Path PROJECT_CONFIG_FOLDER = PROJECT_ROOT.resolve( RETEST_PROJECT_CONFIG_FOLDER );
-	public static final Path COSTUMER_FILTER_FOLDER = PROJECT_CONFIG_FOLDER.resolve( FILTER_FOLDER );
-
 	private static final String BASIC_FILTER_DIR = "/filter/";
 	private static final String WEB_FILTER_DIR = BASIC_FILTER_DIR + "web/";
 	private static final List<String> defaultWebFilter =
 			Arrays.asList( WEB_FILTER_DIR + "positioning.filter", WEB_FILTER_DIR + "visibility.filter" );
 
 	private SearchFilterFiles() {}
+
+	private static Path resolveFilterPath() {
+		final Path PROJECT_ROOT = ProjectRootFinderUtil.getProjectRoot()
+				.orElseThrow( () -> new RuntimeException( "Project root could not be found." ) );
+		final Path PROJECT_CONFIG_FOLDER = PROJECT_ROOT.resolve( RETEST_PROJECT_CONFIG_FOLDER );
+		final Path CUSTOMER_FILTER_FOLDER = PROJECT_CONFIG_FOLDER.resolve( FILTER_FOLDER );
+		return CUSTOMER_FILTER_FOLDER;
+	}
 
 	public static List<File> getDefaultFilterFiles() {
 		return defaultWebFilter.stream() //
@@ -38,15 +42,15 @@ public class SearchFilterFiles {
 				.collect( Collectors.toList() ); //
 	}
 
-	public static List<File> getCostumerFilterFiles() {
-		try ( Stream<Path> paths = Files.walk( COSTUMER_FILTER_FOLDER ) ) {
-			return paths.filter( Files::isRegularFile )//
-					.filter( file -> file.toString().endsWith( ".filter" ) )//
-					.map( Path::toFile )//
-					.collect( Collectors.toList() );
+	public static List<File> getCustomerFilterFiles() {
+		try ( Stream<Path> paths = Files.walk( resolveFilterPath() ) ) {
+			return paths.filter( Files::isRegularFile ) //
+					.filter( file -> file.toString().endsWith( ".filter" ) ) //
+					.map( Path::toFile ) //
+					.collect( Collectors.toList() ); //
 		} catch ( final Exception e ) {
 			log.info( "You can create your own filter files in the \\.retest\\filter folder" );
 		}
-		return null;
+		return new ArrayList<>();
 	}
 }
