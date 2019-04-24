4af3d718b88904d25a83f233d4ba3a0cd0f73564
IgorAlbert <igor.albert@retest.de>
2019-04-24 09:48:00 +0200

    Change test and use temporaryFolder

--- src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
+++ src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
@@ -1,12 +1,16 @@
 package de.retest.recheck.ignore;
 
+import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_CONFIG_FOLDER;
+import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_ROOT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 class SearchFilterFilesTest {
 
@@ -18,21 +22,23 @@ class SearchFilterFilesTest {
 	}
 
 	@Test
-	void getCostumerFilterFiles_should_only_get_filter_files() throws Exception {
-		final File randomFile =
-				Files.createTempFile( SearchFilterFiles.COSTUMER_FILTER_FOLDER, "random", ".ignore" ).toFile();
-		final File colorFilter =
-				Files.createTempFile( SearchFilterFiles.COSTUMER_FILTER_FOLDER, "color", ".filter" ).toFile();
-		final File webFontFilter =
-				Files.createTempFile( SearchFilterFiles.COSTUMER_FILTER_FOLDER, "web-font", ".filter" ).toFile();
+	void getCustomerFilterFiles_should_only_get_filter_files( @TempDir final Path temp ) throws Exception {
+		final Path configFolder = temp.resolve( RETEST_PROJECT_ROOT );
+		Files.createDirectory( configFolder );
+		final Path retestFolder = configFolder.resolve( RETEST_PROJECT_CONFIG_FOLDER );
+		Files.createDirectory( retestFolder );
+		final Path filterFolder = retestFolder.resolve( "filter" );
+		Files.createDirectory( filterFolder );
+		System.setProperty( RETEST_PROJECT_ROOT, filterFolder.toString() );
 
-		final List<File> costumerFilterFiles = SearchFilterFiles.getCostumerFilterFiles();
+		Files.createTempFile( filterFolder, "random", ".ignore" ).toFile();
+		final File colorFilter = Files.createTempFile( filterFolder, "color", ".filter" ).toFile();
+		final File webFontFilter = Files.createTempFile( filterFolder, "web-font", ".filter" ).toFile();
+
+		final List<File> costumerFilterFiles = SearchFilterFiles.getCustomerFilterFiles();
 		assertThat( costumerFilterFiles ).allMatch( file -> file.toString().endsWith( ".filter" ) );
 		assertThat( costumerFilterFiles.stream().map( File::getName ) ).contains( colorFilter.getName().toString(),
 				webFontFilter.getName().toString() );
 
-		colorFilter.deleteOnExit();
-		webFontFilter.deleteOnExit();
-		randomFile.deleteOnExit();
 	}
 }
