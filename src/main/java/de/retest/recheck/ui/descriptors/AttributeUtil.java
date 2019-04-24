6f6a626216b58c5e366395130220ceb92a82b216
Enes Özel <enes.oezel@retest.de>
2019-04-24 14:01:47 +0200

    Add methods for actual outline

--- src/main/java/de/retest/recheck/ui/descriptors/AttributeUtil.java
+++ src/main/java/de/retest/recheck/ui/descriptors/AttributeUtil.java
@@ -2,7 +2,33 @@ package de.retest.recheck.ui.descriptors;
 
 import java.awt.Rectangle;
 
+import de.retest.recheck.ui.diff.AttributeDifference;
+import de.retest.recheck.ui.diff.ElementDifference;
+import de.retest.recheck.ui.diff.IdentifyingAttributesDifference;
+
 public class AttributeUtil {
+
+	public static Rectangle getActualOutline( final ElementDifference difference ) {
+		final IdentifyingAttributesDifference diff =
+				(IdentifyingAttributesDifference) difference.getIdentifyingAttributesDifference();
+		for ( final AttributeDifference aDiff : diff.getAttributeDifferences() ) {
+			if ( aDiff.getKey().equals( OutlineAttribute.RELATIVE_OUTLINE ) ) {
+				return ((Rectangle) aDiff.getActual());
+			}
+		}
+		return getOutline( difference.getIdentifyingAttributes() );
+	}
+
+	public static Rectangle getActualAbsoluteOutline( final ElementDifference difference ) {
+		final IdentifyingAttributesDifference diff =
+				(IdentifyingAttributesDifference) difference.getIdentifyingAttributesDifference();
+		for ( final AttributeDifference aDiff : diff.getAttributeDifferences() ) {
+			if ( aDiff.getKey().equals( OutlineAttribute.ABSOLUTE_OUTLINE ) ) {
+				((OutlineAttribute) aDiff.getActual()).getValue();
+			}
+		}
+		return getAbsoluteOutline( difference.getIdentifyingAttributes() );
+	}
 
 	public static Rectangle getOutline( final IdentifyingAttributes attributes ) {
 		final OutlineAttribute outlineAttribute =
