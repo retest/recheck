6e5ff3315871db9d78c7dfcff24b0dbbbe8c165d
Enes Özel <enes.oezel@retest.de>
2019-04-25 10:37:55 +0200

    Improve methods

--- src/main/java/de/retest/recheck/ui/descriptors/AttributeUtil.java
+++ src/main/java/de/retest/recheck/ui/descriptors/AttributeUtil.java
@@ -1,6 +1,7 @@
 package de.retest.recheck.ui.descriptors;
 
 import java.awt.Rectangle;
+import java.util.Objects;
 
 import de.retest.recheck.ui.diff.AttributeDifference;
 import de.retest.recheck.ui.diff.ElementDifference;
@@ -9,24 +10,32 @@ import de.retest.recheck.ui.diff.IdentifyingAttributesDifference;
 public class AttributeUtil {
 
 	public static Rectangle getActualOutline( final ElementDifference difference ) {
-		final IdentifyingAttributesDifference diff =
+		final IdentifyingAttributesDifference identifyingAttributesDifference =
 				(IdentifyingAttributesDifference) difference.getIdentifyingAttributesDifference();
-		for ( final AttributeDifference aDiff : diff.getAttributeDifferences() ) {
-			if ( aDiff.getKey().equals( OutlineAttribute.RELATIVE_OUTLINE ) ) {
-				return ((Rectangle) aDiff.getActual());
+		try {
+			if ( Objects.nonNull( identifyingAttributesDifference.getAttributeDifferences() ) ) {
+				for ( final AttributeDifference aDiff : identifyingAttributesDifference.getAttributeDifferences() ) {
+					if ( aDiff.getKey().equals( OutlineAttribute.RELATIVE_OUTLINE ) ) {
+						return ((Rectangle) aDiff.getActual());
+					}
+				}
 			}
-		}
+		} catch ( final Exception ignored ) {}
 		return getOutline( difference.getIdentifyingAttributes() );
 	}
 
 	public static Rectangle getActualAbsoluteOutline( final ElementDifference difference ) {
-		final IdentifyingAttributesDifference diff =
+		final IdentifyingAttributesDifference identifyingAttributesDifference =
 				(IdentifyingAttributesDifference) difference.getIdentifyingAttributesDifference();
-		for ( final AttributeDifference aDiff : diff.getAttributeDifferences() ) {
-			if ( aDiff.getKey().equals( OutlineAttribute.ABSOLUTE_OUTLINE ) ) {
-				((OutlineAttribute) aDiff.getActual()).getValue();
+		try {
+			if ( Objects.nonNull( identifyingAttributesDifference.getAttributeDifferences() ) ) {
+				for ( final AttributeDifference aDiff : identifyingAttributesDifference.getAttributeDifferences() ) {
+					if ( aDiff.getKey().equals( OutlineAttribute.ABSOLUTE_OUTLINE ) ) {
+						return ((Rectangle) aDiff.getActual());
+					}
+				}
 			}
-		}
+		} catch ( final Exception ignored ) {}
 		return getAbsoluteOutline( difference.getIdentifyingAttributes() );
 	}
 
@@ -47,4 +56,5 @@ public class AttributeUtil {
 		}
 		return outlineAttribute.getValue();
 	}
+
 }
