55664832cbd78db3f7ce115d2cddfbe489eb5c17
IgorAlbert <igor.albert@retest.de>
2019-05-02 13:39:03 +0200

    Test the behavior of the method

--- src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
+++ src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
@@ -1,11 +1,20 @@
 package de.retest.recheck.ignore;
 
+import static de.retest.recheck.configuration.ProjectConfiguration.FILTER_FOLDER;
+import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_CONFIG_FOLDER;
+import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_ROOT;
+import static de.retest.recheck.ignore.SearchFilterFiles.FILES_ENDING;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import de.retest.recheck.util.junit.jupiter.SystemProperty;
 
 class SearchFilterFilesTest {
 
@@ -14,5 +23,26 @@ class SearchFilterFilesTest {
 		final List<File> defaultFilterFiles = SearchFilterFiles.getDefaultFilterFiles();
 		assertThat( defaultFilterFiles.stream().map( File::getName ) ).contains( "positioning.filter",
 				"visibility.filter" );
+	}
+
+	@Test
+	@SystemProperty( key = RETEST_PROJECT_ROOT )
+	void getProjectFilterFiles_should_only_get_filter_files( @TempDir final Path temp ) throws Exception {
+		final Path configFolder = temp.resolve( RETEST_PROJECT_ROOT );
+		Files.createDirectory( configFolder );
+		final Path retestFolder = configFolder.resolve( RETEST_PROJECT_CONFIG_FOLDER );
+		Files.createDirectory( retestFolder );
+		final Path filterFolder = retestFolder.resolve( FILTER_FOLDER );
+		Files.createDirectory( filterFolder );
+
+		Files.createTempFile( filterFolder, "random", ".ignore" ).toFile();
+		final File colorFilter = Files.createTempFile( filterFolder, "color", FILES_ENDING ).toFile();
+		final File webFontFilter = Files.createTempFile( filterFolder, "web-font", FILES_ENDING ).toFile();
+		System.setProperty( RETEST_PROJECT_ROOT, filterFolder.toString() );
+
+		final List<File> projectFilterFiles = SearchFilterFiles.getProjectFilterFiles();
+		assertThat( projectFilterFiles ).allMatch( file -> file.toString().endsWith( FILES_ENDING ) );
+		assertThat( projectFilterFiles.stream().map( File::getName ) ).contains( colorFilter.getName().toString(),
+				webFontFilter.getName().toString() );
 	}
 }
