f15982aa69b3bb698e0168f26be3020d5b967be4
Jeremias Roessler <roessler@retest.de>
2019-05-05 14:39:30 -0400

    Turn them into actual JS functions...

--- src/main/resources/default-recheck.ignore.js
+++ src/main/resources/default-recheck.ignore.js
@@ -2,8 +2,8 @@
 // Please do not delete this file, even if it is empty.
 
 // You can implement either of these two functions:
-// matches(element)
-// matches(element, diff)
+// function matches(element) {}
+// function matches(element, diff) {}
 
 // You can find more details and example rules at: 
 // https://retest.github.io/docs/recheck/how-ignore-works-in-recheck/
