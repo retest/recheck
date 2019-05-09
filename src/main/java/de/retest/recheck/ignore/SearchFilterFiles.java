89dd3cf59f8e7a7dfbff1a2091e6261939d8c901
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-05-09 13:30:56 +0200

    Implement filter for diff command

--- src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
+++ src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
@@ -1,7 +1,5 @@
 package de.retest.recheck.ignore;
 
-import static de.retest.recheck.configuration.ProjectConfiguration.FILTER_FOLDER;
-
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
@@ -11,16 +9,18 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import de.retest.recheck.configuration.ProjectConfiguration;
+import de.retest.recheck.review.ignore.io.Loaders;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SearchFilterFiles {
 
-	public static final String FILTER_EXTENSION = ".filter";
+	public static final String FILTER_EXTENSION = "filter";
 	private static final String BASIC_FILTER_DIR = "/filter/";
 	private static final String WEB_FILTER_DIR = BASIC_FILTER_DIR + "web/";
 	private static final List<String> defaultWebFilter =
@@ -43,7 +43,7 @@ public class SearchFilterFiles {
 
 	public static List<Path> getProjectFilterFiles() {
 		final Path resolveFilterPath =
-				ProjectConfiguration.getInstance().findProjectConfigFolder().resolve( FILTER_FOLDER );
+				ProjectConfiguration.getInstance().findProjectConfigFolder().resolve( FILTER_EXTENSION );
 		if ( !resolveFilterPath.toFile().exists() ) {
 			return Collections.emptyList();
 		}
@@ -55,5 +55,23 @@ public class SearchFilterFiles {
 			log.error( "Exception accessing user filter folder '{}'.", resolveFilterPath, e );
 			return Collections.emptyList();
 		}
+
+	}
+
+	public static Optional<Filter> searchFilterByName( final String name ) throws IOException {
+		for ( int j = 0; j < getAllFilterFiles().size(); j++ ) {
+			final Path filterPath = getAllFilterFiles().get( j );
+			if ( filterPath.getFileName().toString().equals( name ) ) {
+				log.info( "Loading filter file from path: " + filterPath );
+				try ( final Stream<String> ignoreFileLines = Files.lines( filterPath ) ) {
+					final Filter ignoreApplier = Loaders.load( ignoreFileLines ) //
+							.filter( Filter.class::isInstance ) //
+							.map( Filter.class::cast ) //
+							.collect( Collectors.collectingAndThen( Collectors.toList(), CompoundFilter::new ) );
+					return Optional.of( ignoreApplier );
+				}
+			}
+		}
+		return Optional.empty();
 	}
 }
