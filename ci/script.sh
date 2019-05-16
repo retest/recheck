7d03b8f1aa986533c5edc6c51b4311db518cc59e
varunnayak26 <varun.nayak@retest.de>
2019-05-16 19:32:46 +0200

    Added fix for javadoc fail on jdk11 which essentially results in javadoc being executed with '-source 8' and the javadoc to be generated.

--- ci/script.sh
+++ ci/script.sh
@@ -11,9 +11,8 @@ ${MVN} clean package -DskipTests
 # Test with JDK 11
 wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 11
 
-# We cannot simply use verify due to https://bugs.openjdk.java.net/browse/JDK-8212233.
 if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
-    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent test failsafe:integration-test sonar:sonar
+    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent test verify sonar:sonar
 else
-    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent test failsafe:integration-test
+    ${MVN} org.jacoco:jacoco-maven-plugin:prepare-agent test verify
 fi
