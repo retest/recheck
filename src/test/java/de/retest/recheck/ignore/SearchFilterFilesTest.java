75281fcd2efc8927e003e975c9928f3f600fc863
Daniel Kraus <daniel.kraus@retest.de>
2019-05-21 15:40:42 +0200

    Refactor test

--- src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
+++ src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
@@ -3,16 +3,15 @@ package de.retest.recheck.ignore;
 import static de.retest.recheck.configuration.ProjectConfiguration.FILTER_FOLDER;
 import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_CONFIG_FOLDER;
 import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_ROOT;
-import static de.retest.recheck.ignore.SearchFilterFiles.FILTER_EXTENSION;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,37 +22,41 @@ import de.retest.recheck.util.junit.jupiter.SystemProperty;
 
 class SearchFilterFilesTest {
 
-	private Path filterFolder;
+	Path retestFolder;
+	Path filterFolder;
 
 	@BeforeEach
 	void setUp( @TempDir final Path temp ) throws IOException {
-		final Path configFolder = temp.resolve( RETEST_PROJECT_ROOT );
-		Files.createDirectory( configFolder );
-		final Path retestFolder = configFolder.resolve( RETEST_PROJECT_CONFIG_FOLDER );
-		Files.createDirectory( retestFolder );
+		retestFolder = temp.resolve( RETEST_PROJECT_CONFIG_FOLDER );
 		filterFolder = retestFolder.resolve( FILTER_FOLDER );
-		Files.createDirectory( filterFolder );
+		Files.createDirectories( filterFolder );
 	}
 
 	@Test
-	void getDefaultFilterFiles_should_get_all_filter_files_from_classpath() {
+	void getDefaultFilterFiles_should_get_all_predefines_filters() {
 		final List<Pair<String, FilterLoader>> defaultFilterFiles = SearchFilterFiles.getDefaultFilterFiles();
-		assertThat( defaultFilterFiles.stream().map( Pair::getLeft ) ).contains( "positioning.filter",
-				"visibility.filter" );
+		final List<String> actualFilterFileNames = defaultFilterFiles.stream() //
+				.map( Pair::getLeft ) //
+				.collect( Collectors.toList() );
+		final List<String> expectedFilterFileNames = Arrays.asList( "positioning.filter", "visibility.filter" );
+		assertThat( actualFilterFileNames ).isEqualTo( expectedFilterFileNames );
 	}
 
 	@Test
 	@SystemProperty( key = RETEST_PROJECT_ROOT )
-	void getProjectFilterFiles_should_only_get_filter_files() throws IOException {
-		Files.createTempFile( filterFolder, "random", ".ignore" ).toFile();
-		final File colorFilter = Files.createTempFile( filterFolder, "color", FILTER_EXTENSION ).toFile();
-		final File webFontFilter = Files.createTempFile( filterFolder, "web-font", FILTER_EXTENSION ).toFile();
+	void getProjectFilterFiles_should_get_all_user_defined_filters() throws IOException {
 		System.setProperty( RETEST_PROJECT_ROOT, filterFolder.toString() );
+		final Path someFilter = filterFolder.resolve( "some.filter" );
+		Files.createFile( someFilter );
+		final Path anotherFilter = filterFolder.resolve( "another.filter" );
+		Files.createFile( anotherFilter );
 
 		final List<Pair<String, FilterLoader>> projectFilterFiles = SearchFilterFiles.getProjectFilterFiles();
-		assertThat( projectFilterFiles ).allMatch( pair -> pair.getLeft().endsWith( FILTER_EXTENSION ) );
-		assertThat( projectFilterFiles.stream().map( Pair::getLeft ) ).contains(
-				colorFilter.toPath().getFileName().toString(), webFontFilter.toPath().getFileName().toString() );
+		final List<String> actualFilterFileNames = projectFilterFiles.stream() //
+				.map( Pair::getLeft ) //
+				.collect( Collectors.toList() );
+		final List<String> expectedFilterFileNames = Arrays.asList( "another.filter", "some.filter" );
+		assertThat( actualFilterFileNames ).isEqualTo( expectedFilterFileNames );
 	}
 
 	@Test
