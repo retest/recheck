e23d04725919147854e25a6ac4d85ed1db564449
Daniel Kraus <daniel.kraus@retest.de>
2019-05-23 10:53:05 +0200

    Log possible regex warning

--- src/main/java/de/retest/recheck/review/ignore/AttributeFilter.java
+++ src/main/java/de/retest/recheck/review/ignore/AttributeFilter.java
@@ -4,9 +4,11 @@ import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 import de.retest.recheck.ignore.Filter;
+import de.retest.recheck.review.ignore.AttributeRegexFilter.AttributeRegexFilterLoader;
 import de.retest.recheck.review.ignore.io.RegexLoader;
 import de.retest.recheck.ui.descriptors.Element;
 import de.retest.recheck.ui.diff.AttributeDifference;
+import lombok.extern.slf4j.Slf4j;
 
 public class AttributeFilter implements Filter {
 
@@ -31,12 +33,15 @@ public class AttributeFilter implements Filter {
 		return String.format( AttributeFilterLoader.FORMAT, attribute );
 	}
 
+	@Slf4j
 	public static class AttributeFilterLoader extends RegexLoader<AttributeFilter> {
 
 		static final String KEY = "attribute=";
 
 		private static final String FORMAT = KEY + "%s";
 		private static final Pattern REGEX = Pattern.compile( KEY + "(.+)" );
+
+		private static final String POSSIBLE_REGEX = "*";
 
 		public AttributeFilterLoader() {
 			super( REGEX );
@@ -45,6 +50,12 @@ public class AttributeFilter implements Filter {
 		@Override
 		protected AttributeFilter load( final MatchResult regex ) {
 			final String attribute = regex.group( 1 );
+			if ( attribute.contains( POSSIBLE_REGEX ) ) {
+				final String actualLine = KEY + attribute;
+				final String suggestedLine = AttributeRegexFilterLoader.KEY + attribute;
+				log.warn( "'{}' contains '{}'. For regular expressions, please use '{}'.", actualLine, POSSIBLE_REGEX,
+						suggestedLine );
+			}
 			return new AttributeFilter( attribute );
 		}
 	}
