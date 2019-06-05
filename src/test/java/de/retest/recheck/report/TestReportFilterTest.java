ac3e02c9da0c4f7bca534cd18b3ceeca576b9dee
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-06-05 14:34:21 +0200

    Make lokal variables

--- src/test/java/de/retest/recheck/report/TestReportFilterTest.java
+++ src/test/java/de/retest/recheck/report/TestReportFilterTest.java
@@ -74,8 +74,8 @@ class TestReportFilterTest {
 		when( originalElementDifference.getIdentifyingAttributes().identifier() ).thenReturn( "identifier" );
 		originalRootElementDifference = new RootElementDifference( originalElementDifference, mock( RootElement.class ),
 				mock( RootElement.class ) );
-		secondRootElementDifference = new RootElementDifference( originalElementDifference, mock( RootElement.class ),
-				mock( RootElement.class ) );
+		final RootElementDifference secondRootElementDifference = new RootElementDifference( originalElementDifference,
+				mock( RootElement.class ), mock( RootElement.class ) );
 		originalRootElementDifferences = Arrays.asList( originalRootElementDifference, secondRootElementDifference );
 		originalStateDifference =
 				new StateDifference( originalRootElementDifferences, mock( DurationDifference.class ) );
@@ -84,7 +84,7 @@ class TestReportFilterTest {
 		originalTestReplayResult = new TestReplayResult( "test", 1 );
 		originalTestReplayResult.addAction( originalActionReplayResult );
 		originalSuiteReplayResult =
-				new SuiteReplayResult( mock( ExecutableSuite.class ), 1, mock( GroundState.class ) );
+				new SuiteReplayResult( "", 0, mock( ExecutableSuite.class ), "", mock( GroundState.class ) );
 		originalSuiteReplayResult.addTest( originalTestReplayResult );
 		originalTestReport = new TestReport();
 		originalTestReport.addSuite( originalSuiteReplayResult );
@@ -138,48 +138,55 @@ class TestReportFilterTest {
 		when( originalElementDifference.getIdentifyingAttributes().identifier() ).thenReturn( "identifier" );
 		final RootElementDifference filteredRootElementDifference =
 				TestReportFilter.filter( originalRootElementDifference, filter );
-		assertThat( filteredRootElementDifference.getElementDifference().getAttributesDifference().getDifferences() )
-				.containsExactly( notFilterMe );
+		final List<AttributeDifference> differences =
+				filteredRootElementDifference.getElementDifference().getAttributesDifference().getDifferences();
+		assertThat( differences ).containsExactly( notFilterMe );
 	}
 
 	@Test
 	void list_of_root_element_differences_should_be_filtered_properly() throws Exception {
 		final List<RootElementDifference> filteredRootElementDifferences =
 				TestReportFilter.filter( originalRootElementDifferences, filter );
-		assertThat( filteredRootElementDifferences.get( 0 ).getElementDifference().getAttributesDifference()
-				.getDifferences() ).contains( notFilterMe );
+		List<AttributeDifference> differences = filteredRootElementDifferences.get( 0 ).getElementDifference().getAttributesDifference()
+				.getDifferences();
+		assertThat( differences ).contains( notFilterMe );
 	}
 
 	@Test
 	void state_difference_should_be_filtered_properly() throws Exception {
 		final StateDifference filteredStateDifference = TestReportFilter.filter( originalStateDifference, filter );
-		assertThat( filteredStateDifference.getRootElementDifferences().get( 0 ).getElementDifference()
-				.getAttributesDifference().getDifferences() ).containsExactly( notFilterMe );
+		List<AttributeDifference> differences = filteredStateDifference.getRootElementDifferences().get( 0 ).getElementDifference()
+				.getAttributesDifference().getDifferences();
+		assertThat( differences ).containsExactly( notFilterMe );
 	}
 
 	@Test
 	void action_replay_result_should_be_filtered_properly() throws Exception {
 		final ActionReplayResult filteredActionReplayResult =
 				TestReportFilter.filter( originalActionReplayResult, filter );
-		assertThat( filteredActionReplayResult.getStateDifference().getRootElementDifferences().get( 0 )
-				.getElementDifference().getAttributesDifference().getDifferences() ).containsExactly( notFilterMe );
+		List<AttributeDifference> differences = filteredActionReplayResult.getStateDifference().getRootElementDifferences().get( 0 )
+				.getElementDifference().getAttributesDifference().getDifferences();
+		assertThat( differences ).containsExactly( notFilterMe );
 	}
 
 	@Test
 	void test_replay_result_should_be_filtered_properly() throws Exception {
 		final TestReplayResult filteredTestReplayResult = TestReportFilter.filter( originalTestReplayResult, filter );
-		assertThat( filteredTestReplayResult.getActionReplayResults().get( 0 ).getStateDifference()
+		List<AttributeDifference> differences = filteredTestReplayResult.getActionReplayResults().get( 0 ).getStateDifference()
 				.getRootElementDifferences().get( 0 ).getElementDifference().getAttributesDifference()
-				.getDifferences() ).containsExactly( notFilterMe );
+				.getDifferences();
+		assertThat( differences ).containsExactly( notFilterMe );
 	}
 
 	@Test
 	void suite_replay_result_should_be_filtered_properly() throws Exception {
 		final SuiteReplayResult filteredSuiteReplayResult =
 				TestReportFilter.filter( originalSuiteReplayResult, filter );
-		assertThat( filteredSuiteReplayResult.getTestReplayResults().get( 0 ).getActionReplayResults().get( 0 )
-				.getStateDifference().getRootElementDifferences().get( 0 ).getElementDifference()
-				.getAttributesDifference().getDifferences() ).containsExactly( notFilterMe );
+		StateDifference stateDifference = filteredSuiteReplayResult.getTestReplayResults().get( 0 ).getActionReplayResults().get( 0 )
+				.getStateDifference();
+		List<AttributeDifference> differences = stateDifference.getRootElementDifferences().get( 0 ).getElementDifference()
+				.getAttributesDifference().getDifferences();
+		assertThat( differences ).containsExactly( notFilterMe );
 	}
 
 	@Test
@@ -188,7 +195,8 @@ class TestReportFilterTest {
 		final SuiteReplayResult suiteReplayResult = filteredTestReport.getSuiteReplayResults().get( 0 );
 		final ActionReplayResult actionReplayResult =
 				suiteReplayResult.getTestReplayResults().get( 0 ).getActionReplayResults().get( 0 );
-		assertThat( actionReplayResult.getStateDifference().getRootElementDifferences().get( 0 ).getElementDifference()
-				.getAttributesDifference().getDifferences() ).containsExactly( notFilterMe );
+		List<AttributeDifference> differences = actionReplayResult.getStateDifference().getRootElementDifferences().get( 0 ).getElementDifference()
+				.getAttributesDifference().getDifferences();
+		assertThat( differences ).containsExactly( notFilterMe );
 	}
 }
