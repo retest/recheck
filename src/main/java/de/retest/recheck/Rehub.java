261b61ee6ac76aec61e8899c045ea13766353130
Johannes Dillmann <johannes.dillmann@retest.de>
2019-06-14 14:53:03 +0200

    Add rehub initialization

---
+++ src/main/java/de/retest/recheck/Rehub.java
@@ -0,0 +1,31 @@
+package de.retest.recheck;
+
+import java.util.prefs.Preferences;
+
+import de.retest.recheck.auth.RehubAuthenticationHandler;
+import de.retest.recheck.auth.RetestAuthentication;
+import de.retest.recheck.persistence.CloudPersistence;
+
+public class Rehub {
+
+	private Rehub() {
+
+	}
+
+	public static void init() {
+		final RetestAuthentication auth = RetestAuthentication.getInstance();
+
+		if ( !auth.isAuthenticated( getToken() ) ) {
+			auth.login( new RehubAuthenticationHandler() );
+		}
+	}
+
+	private static String getToken() {
+		final String tokenFromEnvironment = System.getenv( CloudPersistence.RECHECK_API_KEY );
+		final String tokenFromPreferences =
+				Preferences.userNodeForPackage( Rehub.class ).get( CloudPersistence.RECHECK_API_KEY, null );
+
+		return tokenFromEnvironment != null ? tokenFromEnvironment : tokenFromPreferences;
+	}
+
+}
