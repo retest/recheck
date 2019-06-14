8883778d7dca9804315ed6a903a3dd99e4e80fef
Johannes Dillmann <johannes.dillmann@retest.de>
2019-06-14 14:49:50 +0200

    Split output format for state and report

--- src/main/java/de/retest/recheck/Properties.java
+++ src/main/java/de/retest/recheck/Properties.java
@@ -5,6 +5,8 @@ public class Properties {
 	public static final String FILE_OUTPUT_FORMAT_PROPERTY = "de.retest.output.Format";
 	public static final String CONFIG_FILE_PROPERTY = "de.retest.configFile";
 
+	public static final String REHUB_ENABLED = "de.retest.recheck.rehub.enabled";
+
 	public static final String PROPERTY_VALUE_SEPARATOR = ";";
 	public static final String ZIP_FOLDER_SEPARATOR = "/";
 
@@ -26,10 +28,19 @@ public class Properties {
 	public static enum FileOutputFormat {
 		PLAIN,
 		ZIP,
-		KRYO
+		KRYO,
+		CLOUD
 	}
 
-	public static FileOutputFormat getFileOutputFormat() {
+	public static FileOutputFormat getReportOutputFormat() {
+		final String format = System.getProperty( FILE_OUTPUT_FORMAT_PROPERTY, FileOutputFormat.KRYO.toString() );
+		if ( Boolean.getBoolean( REHUB_ENABLED ) ) {
+			return FileOutputFormat.CLOUD;
+		}
+		return FileOutputFormat.valueOf( format );
+	}
+
+	public static FileOutputFormat getStateOutputFormat() {
 		final String format = System.getProperty( FILE_OUTPUT_FORMAT_PROPERTY, FileOutputFormat.PLAIN.toString() );
 		if ( format.equalsIgnoreCase( FileOutputFormat.ZIP.toString() ) ) {
 			return FileOutputFormat.ZIP;
