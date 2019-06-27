aa23bdacb9e1dd9dcdd2360b9d7410f77a4021bc
Daniel Kraus <daniel.kraus@retest.de>
2019-06-27 09:39:53 +0200

    Add class for GUI element identification warning

---
+++ src/main/java/de/retest/recheck/ui/diff/ElementIdentificationWarning.java
@@ -0,0 +1,36 @@
+package de.retest.recheck.ui.diff;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Structurally stores the information about a test breakage caused by the
+ * <a href="https://retest.github.io/docs/recheck-web/element-identification-problem/">GUI element identification
+ * problem</a>. This information can be re-used by review or recheck.cli when the user applies a change.
+ */
+@Data
+@RequiredArgsConstructor
+public class ElementIdentificationWarning implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@XmlAttribute
+	private final String elementIdentifier;
+
+	@XmlAttribute
+	private final String testClassName;
+
+	@XmlAttribute
+	private final String retestId;
+
+	@SuppressWarnings( "unused" )
+	private ElementIdentificationWarning() {
+		elementIdentifier = null;
+		testClassName = null;
+		retestId = null;
+	}
+}
