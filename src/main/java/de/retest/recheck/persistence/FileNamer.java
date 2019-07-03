d1a9540067f5cb3703165f94d7b8788700e5235c
Daniel Kraus <daniel.kraus@retest.de>
2019-07-03 18:46:15 +0200

    Remove unnecessary modifiers

--- src/main/java/de/retest/recheck/persistence/FileNamer.java
+++ src/main/java/de/retest/recheck/persistence/FileNamer.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 public interface FileNamer {
 
-	public File getFile( String extension );
+	File getFile( String extension );
 
-	public File getResultFile( String extension );
+	File getResultFile( String extension );
 
 }
