e27db6925b2777183a1805473861798fdf888e19
Jeremias Roessler <roessler@retest.de>
2019-08-26 13:21:45 +0200

    Have the filters used as option and use manual builder

--- src/main/java/de/retest/recheck/RecheckOptions.java
+++ src/main/java/de/retest/recheck/RecheckOptions.java
@@ -1,24 +1,91 @@
 package de.retest.recheck;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.experimental.SuperBuilder;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-@SuperBuilder
+import de.retest.recheck.ignore.CompoundFilter;
+import de.retest.recheck.ignore.Filter;
+import de.retest.recheck.ignore.RecheckIgnoreUtil;
+import lombok.Getter;
+
 @Getter
 public class RecheckOptions {
 
-	@Builder.Default
-	private final FileNamerStrategy fileNamerStrategy = new MavenConformFileNamerStrategy();
+	private final FileNamerStrategy fileNamerStrategy;
+	private final String suiteName;
+	private final boolean reportUploadEnabled;
+	private final Filter filter;
 
-	@Builder.Default
-	private final String suiteName = null;
-
-	@Builder.Default
-	private final boolean reportUploadEnabled = false;
+	public RecheckOptions( final FileNamerStrategy fileNamerStrategy, final String suiteName,
+			final boolean reportUploadEnabled, final Filter filter ) {
+		this.fileNamerStrategy = fileNamerStrategy;
+		this.suiteName = suiteName;
+		this.reportUploadEnabled = reportUploadEnabled;
+		this.filter = filter;
+	}
 
 	public String getSuiteName() {
 		return suiteName == null ? fileNamerStrategy.getTestClassName() : suiteName;
 	}
 
+	public static RecheckOptionsBuilder builder() {
+		return new RecheckOptionsBuilder();
+	}
+
+	public static class RecheckOptionsBuilder {
+		private FileNamerStrategy fileNamerStrategy = new MavenConformFileNamerStrategy();
+		private String suiteName = null;
+		private boolean reportUploadEnabled = false;
+		private Filter filter = null;
+		private final List<Filter> filterToAdd =
+				new ArrayList<>( Arrays.asList( RecheckIgnoreUtil.loadRecheckIgnore() ) );
+		private boolean addSuiteFilter = true;
+
+		public RecheckOptionsBuilder fileNamerStrategy( final FileNamerStrategy fileNamerStrategy ) {
+			this.fileNamerStrategy = fileNamerStrategy;
+			return this;
+		}
+
+		public RecheckOptionsBuilder suiteName( final String suiteName ) {
+			this.suiteName = suiteName;
+			return this;
+		}
+
+		public RecheckOptionsBuilder reportUploadEnabled( final boolean reportUploadEnabled ) {
+			this.reportUploadEnabled = reportUploadEnabled;
+			return this;
+		}
+
+		public RecheckOptionsBuilder filter( final Filter filter ) {
+			addSuiteFilter = false;
+			this.filter = filter;
+			return this;
+		}
+
+		public RecheckOptionsBuilder addFilter( final Filter added ) {
+			if ( filter instanceof CompoundFilter ) {
+				filterToAdd.add( added );
+			} else {
+				throw new IllegalStateException(
+						"Can only combine `filter()` and `add(filter)` if the set filter is of type `CompoundFilter`." );
+			}
+			return this;
+		}
+
+		private File getSuitePath() {
+			return fileNamerStrategy.createFileNamer( suiteName ).getFile( "" );
+		}
+
+		public RecheckOptions build() {
+			if ( addSuiteFilter ) {
+				filterToAdd.add( RecheckIgnoreUtil.loadRecheckSuiteIgnore( getSuitePath() ) );
+			}
+			if ( filter == null ) {
+				filter = new CompoundFilter( filter );
+			}
+			return new RecheckOptions( fileNamerStrategy, suiteName, reportUploadEnabled, filter );
+		}
+	}
 }
