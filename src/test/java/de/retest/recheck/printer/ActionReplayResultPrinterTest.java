d6b8748c9f49c011d2e17232792a4b6c7a24a668
diba1013 <bastian.diehl@retest.de>
2019-08-28 14:43:43 +0200

    Add test to describe verbose deletion cascade

--- src/test/java/de/retest/recheck/printer/ActionReplayResultPrinterTest.java
+++ src/test/java/de/retest/recheck/printer/ActionReplayResultPrinterTest.java
@@ -4,19 +4,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import de.retest.recheck.NoGoldenMasterActionReplayResult;
 import de.retest.recheck.report.ActionReplayResult;
+import de.retest.recheck.report.action.ActionReplayData;
+import de.retest.recheck.report.action.DifferenceRetriever;
+import de.retest.recheck.report.action.WindowRetriever;
+import de.retest.recheck.ui.Path;
 import de.retest.recheck.ui.actions.ExceptionWrapper;
 import de.retest.recheck.ui.actions.TargetNotFoundException;
+import de.retest.recheck.ui.descriptors.Element;
 import de.retest.recheck.ui.descriptors.IdentifyingAttributes;
+import de.retest.recheck.ui.descriptors.PathAttribute;
 import de.retest.recheck.ui.descriptors.RootElement;
 import de.retest.recheck.ui.descriptors.SutState;
+import de.retest.recheck.ui.diff.AttributeDifference;
+import de.retest.recheck.ui.diff.AttributesDifference;
 import de.retest.recheck.ui.diff.ElementDifference;
+import de.retest.recheck.ui.diff.InsertedDeletedElementDifference;
+import de.retest.recheck.ui.diff.RootElementDifference;
+import de.retest.recheck.util.ApprovalsUtil;
 
 class ActionReplayResultPrinterTest {
 
@@ -103,5 +116,67 @@ class ActionReplayResultPrinterTest {
 		final String string = cut.toString( result );
 
 		assertThat( string ).contains( NoGoldenMasterActionReplayResult.MSG_LONG );
+	}
+
+	@Test
+	void toString_should_not_print_child_differences_if_insertion_or_deletion() {
+		final List<ElementDifference> empty = Collections.emptyList();
+
+		final ElementDifference differences = change( Path.fromString( "html[1]/body[1]" ), Arrays.asList( //
+				delete( Path.fromString( "html[1]/body[1]/div[1]" ), Arrays.asList( // 
+						delete( Path.fromString( "html[1]/body[1]/div[1]/div[1]" ), Arrays.asList( // 
+								delete( Path.fromString( "html[1]/body[1]/div[1]/div[1]/div[1]" ), empty ), // 
+								delete( Path.fromString( "html[1]/body[1]/div[1]/div[1]/div[2]" ), empty ) //
+						) ), //
+						delete( Path.fromString( "html[1]/body[1]/div[1]/div[2]" ), empty ) //
+				) ), //
+				change( Path.fromString( "html[1]/body[1]/div[2]" ), Arrays.asList( // 
+						delete( Path.fromString( "html[1]/body[1]/div[2]/div[1]" ), Arrays.asList( // 
+								delete( Path.fromString( "html[1]/body[1]/div[2]/div[1]/div[1]" ), empty ), //
+								delete( Path.fromString( "html[1]/body[1]/div[2]/div[1]/div[2]" ), empty ) //
+						) ), //
+						change( Path.fromString( "html[1]/body[1]/div[2]/div[2]" ), empty ) ) //
+				) ) );
+
+		final RootElementDifference rootDifference =
+				new RootElementDifference( differences, mock( RootElement.class ), mock( RootElement.class ) );
+
+		final ActionReplayResult replayResult = ActionReplayResult.withDifference( ActionReplayData.ofSutStart(),
+				WindowRetriever.empty(), DifferenceRetriever.of( Collections.singletonList( rootDifference ) ), 0L );
+
+		ApprovalsUtil.verify( cut.toString( replayResult ) );
+	}
+
+	private ElementDifference delete( final Path path, final List<ElementDifference> childDifferences ) {
+		final Element element = element( path );
+
+		final InsertedDeletedElementDifference insertion =
+				InsertedDeletedElementDifference.differenceFor( element, null );
+
+		return new ElementDifference( element, null, insertion, null, null, childDifferences );
+	}
+
+	private ElementDifference change( final Path path, final List<ElementDifference> childDifferences ) {
+		final Element element = element( path );
+
+		final AttributesDifference attributes = new AttributesDifference( Arrays.asList( // 
+				new AttributeDifference( "foo-1", "bar-1", "bar1" ), //
+				new AttributeDifference( "foo-2", "bar-2", "bar2" ), //
+				new AttributeDifference( "foo-3", "bar-3", "bar3" ) //
+		) );
+
+		return new ElementDifference( element, attributes, null, null, null, childDifferences );
+	}
+
+	private Element element( final Path path ) {
+		final PathAttribute pathAttribute = new PathAttribute( path );
+
+		final IdentifyingAttributes identifyingAttributes = mock( IdentifyingAttributes.class );
+		when( identifyingAttributes.getPath() ).thenReturn( path.toString() );
+		when( identifyingAttributes.toString() ).thenReturn( path.getElement().toString() );
+
+		final Element element = mock( Element.class );
+		when( element.getIdentifyingAttributes() ).thenReturn( identifyingAttributes );
+		return element;
 	}
 }
