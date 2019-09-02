03426b1dcd13b87788d2342ad426f73ab3498d78
Jeremias Roessler <roessler@retest.de>
2019-09-02 14:06:23 +0200

    Refactor to improve test

--- src/test/java/de/retest/recheck/persistence/bin/KryoPersistenceTest.java
+++ src/test/java/de/retest/recheck/persistence/bin/KryoPersistenceTest.java
@@ -2,6 +2,7 @@ package de.retest.recheck.persistence.bin;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -14,7 +15,6 @@ import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-import org.mockito.Mockito;
 
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.KryoException;
@@ -31,13 +31,17 @@ class KryoPersistenceTest {
 		final URI identifier = file.toUri();
 
 		final KryoPersistence<de.retest.recheck.test.Test> kryoPersistence = new KryoPersistence<>();
-		final ArrayList<String> tests = new ArrayList<>();
-		tests.add( "../test.test" );
-		final de.retest.recheck.test.Test persisted = new de.retest.recheck.test.Test( tests );
+		final de.retest.recheck.test.Test persisted = createDummyTest();
 		kryoPersistence.save( identifier, persisted );
 		final de.retest.recheck.test.Test loaded = kryoPersistence.load( identifier );
 
 		assertThat( persisted.getRelativeActionSequencePaths() ).isEqualTo( loaded.getRelativeActionSequencePaths() );
+	}
+
+	public de.retest.recheck.test.Test createDummyTest() {
+		final ArrayList<String> tests = new ArrayList<>();
+		tests.add( "../test.test" );
+		return new de.retest.recheck.test.Test( tests );
 	}
 
 	@Test
@@ -47,12 +51,10 @@ class KryoPersistenceTest {
 		final URI identifier = file.toUri();
 
 		final KryoPersistence<de.retest.recheck.test.Test> kryoPersistence = new KryoPersistence<>();
-		final ArrayList<String> tests = new ArrayList<>();
-		tests.add( "../test.test" );
-		kryoPersistence.save( identifier, new de.retest.recheck.test.Test( tests ) );
+		kryoPersistence.save( identifier, createDummyTest() );
 
 		final Kryo kryoMock = mock( Kryo.class );
-		when( kryoMock.readClassAndObject( Mockito.any() ) ).thenThrow( KryoException.class );
+		when( kryoMock.readClassAndObject( any() ) ).thenThrow( KryoException.class );
 		final KryoPersistence<de.retest.recheck.test.Test> differentKryoPersistence =
 				new KryoPersistence<>( kryoMock, "old Version" );
 
