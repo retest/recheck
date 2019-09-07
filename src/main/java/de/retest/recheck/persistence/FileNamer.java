ac19a39e69362ae11c263a3c72a952f451f502bb
Jeremias Roessler <roessler@retest.de>
2019-09-08 00:09:37 +0200

    Replace semantically wrong and hard to implement methods with better ones

--- src/main/java/de/retest/recheck/persistence/FileNamer.java
+++ src/main/java/de/retest/recheck/persistence/FileNamer.java
@@ -2,23 +2,39 @@ package de.retest.recheck.persistence;
 
 import java.io.File;
 
+import de.retest.recheck.Properties;
+
 /**
  * Provides file names, respectively, file paths for both Golden Masters and test reports.
  */
 public interface FileNamer {
 
 	/**
-	 * @param extension
-	 *            The file extension to be used.
-	 * @return The Golden Master file.
+	 * Get the GoldenMaster file for the current check, which this interface makes no assumptions about how it is
+	 * contrived.
 	 */
+	default File getGoldenMaster( final String suitename, final String testname, final String checkname ) {
+		return getFile( Properties.GOLDEN_MASTER_FILE_EXTENSION );
+	}
+
+	/**
+	 * @deprecated Because the extension for the Golden Master is fixed
+	 */
+	@Deprecated
 	File getFile( String extension );
 
 	/**
-	 * @param extension
-	 *            The file extension to be used.
-	 * @return The test report file.
+	 * Get the result file for the current suite, which this interface makes no assumptions about how it is contrived.
 	 */
-	File getResultFile( String extension );
+	default File getReport( final String suitename ) {
+		return getResultFile( Properties.TEST_REPORT_FILE_EXTENSION );
+	}
+
+	/**
+	 * @deprecated Because the extension for the result files is fixed and this method is hard to implement/override
+	 *             without explicitly passing a suitename. Use {@link #getReport(String)} instead.
+	 */
+	@Deprecated
+	File getResultFile( final String extension );
 
 }
