a319f726793aa0eb0830302287e6f0161ee55a8a
Lars Briem <lars.briem@kit.edu>
2019-09-09 15:26:31 +0200

    Separate lifecycle methods of Recheck into interface.

---
+++ src/main/java/de/retest/recheck/RecheckLifecycle.java
@@ -0,0 +1,30 @@
+package de.retest.recheck;
+
+public interface RecheckLifecycle {
+
+	/**
+	 * Start a test and infer the test name from the calling method.
+	 */
+	void startTest();
+
+	/**
+	 * Tells recheck that a test has started. This helps to check multiple states within a single test.
+	 *
+	 * @param testName
+	 *            the name of the test
+	 */
+	void startTest( String testName );
+
+	/**
+	 * Tells recheck that the test has finished. If one or more checks during this tests resulted in differences, this
+	 * triggers an {@link AssertionError} to be thrown like any other assertion would. Still, the differences are
+	 * persisted as test reports into the target folder.
+	 */
+	void capTest() throws AssertionError;
+
+	/**
+	 * Finishes recheck and persists the test report. Starting the review GUI is sensible.
+	 */
+	void cap();
+
+}
