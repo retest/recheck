eae279da55f2f8c4127a8433b7d83c5a8a449f5a
Pascal Allion <pascal.allion@retest.de>
2019-09-11 10:18:23 +0200

    Add output if no differences found
    (cherry picked from commit f44afe2765bbe05dea64c1712922f94f56027e35)

--- src/test/java/de/retest/recheck/printer/TestReportPrinterTest.java
+++ src/test/java/de/retest/recheck/printer/TestReportPrinterTest.java
@@ -1,10 +1,11 @@
 package de.retest.recheck.printer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,15 +15,32 @@ import de.retest.recheck.report.TestReport;
 class TestReportPrinterTest {
 
 	@Test
-	void toString_should_not_print_if_no_differences() {
-		final SuiteReplayResult emptySuite = mock( SuiteReplayResult.class );
-		when( emptySuite.isEmpty() ).thenReturn( true );
-
+	void toString_should_print_if_no_differences() {
 		final TestReport testReport = mock( TestReport.class );
-		when( testReport.getSuiteReplayResults() ).thenReturn( Collections.singletonList( emptySuite ) );
+		when( testReport.isEmpty() ).thenReturn( true );
 
 		final TestReportPrinter cut = new TestReportPrinter( DefaultValueFinderProvider.none() );
 
-		assertThat( cut.toString( testReport ) ).isEmpty();
+		assertThat( cut.toString( testReport ) ).isEqualTo( "No differences found." );
+	}
+
+	@Test
+	void toString_should_filter_empty_suites() {
+		final SuiteReplayResult emptySuite = mock( SuiteReplayResult.class );
+		when( emptySuite.isEmpty() ).thenReturn( true );
+
+		final SuiteReplayResult notEmptySuite = mock( SuiteReplayResult.class );
+		when( notEmptySuite.isEmpty() ).thenReturn( false );
+
+		final TestReport testReport = mock( TestReport.class );
+		when( testReport.getSuiteReplayResults() ).thenReturn( Arrays.asList( emptySuite, notEmptySuite ) );
+
+		final SuiteReplayResultPrinter delegate = mock( SuiteReplayResultPrinter.class );
+		final String toStringPerSuite = "foo bar baz";
+		when( delegate.toString( any(), any() ) ).thenReturn( toStringPerSuite );
+
+		final TestReportPrinter cut = new TestReportPrinter( delegate );
+
+		assertThat( cut.toString( testReport ) ).as( "should filter empty suites" ).isEqualTo( toStringPerSuite );
 	}
 }
