7d210e4c406f542ed818cfd713385867ec0cae27
Lars Briem <lars.briem@briemla.de>
2019-09-23 16:13:01 +0200

    Move evaluation of suite name into builder.
    Revert RecheckOptions to simple data holder.

--- src/test/java/de/retest/recheck/RecheckOptionsTest.java
+++ src/test/java/de/retest/recheck/RecheckOptionsTest.java
@@ -2,7 +2,9 @@ package de.retest.recheck;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
@@ -13,11 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import de.retest.recheck.RecheckOptions.RecheckOptionsBuilder;
 import de.retest.recheck.ignore.CompoundFilter;
 import de.retest.recheck.ignore.Filter;
 import de.retest.recheck.persistence.ClassAndMethodBasedShortNamingStrategy;
 import de.retest.recheck.persistence.FileNamer;
 import de.retest.recheck.persistence.NamingStrategy;
+import de.retest.recheck.persistence.ProjectLayout;
 
 class RecheckOptionsTest {
 
@@ -26,14 +30,14 @@ class RecheckOptionsTest {
 		final RecheckOptions cut = RecheckOptions.builder() //
 				.fileNamerStrategy( new MavenConformFileNamerStrategy() ) //
 				.build();
-		assertThat( cut.getSuiteName() ).isEqualTo( getClass().getName() );
+		assertThat( cut.getNamingStrategy().getSuiteName() ).isEqualTo( getClass().getName() );
 	}
 
 	@Test
 	void should_use_NamingStrategy_for_suite_name() throws Exception {
 		final RecheckOptions cut = RecheckOptions.builder() //
 				.namingStrategy( new ClassAndMethodBasedShortNamingStrategy() ).build();
-		assertThat( cut.getSuiteName() ).isEqualTo( getClass().getSimpleName() );
+		assertThat( cut.getNamingStrategy().getSuiteName() ).isEqualTo( getClass().getSimpleName() );
 	}
 
 	@Test
@@ -41,7 +45,7 @@ class RecheckOptionsTest {
 		final RecheckOptions cut = RecheckOptions.builder() //
 				.suiteName( "bar" ) //
 				.build();
-		assertThat( cut.getSuiteName() ).isEqualTo( "bar" );
+		assertThat( cut.getNamingStrategy().getSuiteName() ).isEqualTo( "bar" );
 	}
 
 	@Test
@@ -74,18 +78,31 @@ class RecheckOptionsTest {
 				.isEqualTo( "# Style attributes filter file for recheck." );
 	}
 
-	@ParameterizedTest
-	@MethodSource( "suiteNames" )
-	void suiteName_should_always_overwrite_the_naming_strategy( final RecheckOptions options ) {
-		assertThat( options.getSuiteName() ).isEqualTo( "name" );
-		assertThat( options.getSuitePath() ).isEqualTo( new File( "src/test/resources/retest/recheck/name" ) );
+	@Test
+	void suiteName_should_always_overwrite_the_naming_strategy() {
+		final RecheckOptionsBuilder builder = RecheckOptions.builder().suiteName( "name" );
+		assertThat( builder.build().getNamingStrategy().getSuiteName() ).isEqualTo( "name" );
 	}
 
-	static Stream<RecheckOptions> suiteNames() {
-		final NamingStrategy namingStrategy = mock( NamingStrategy.class );
-		when( namingStrategy.getSuiteName() ).thenReturn( "suite" );
-		when( namingStrategy.getTestName() ).thenReturn( "test" );
+	@ParameterizedTest
+	@MethodSource( "suiteNamesFileNameStrategy" )
+	void suiteName_should_always_overwrite_the_naming_strategy_of_file_namer_strategy(
+			final RecheckOptionsBuilder builder ) {
+		final RecheckOptions options = builder.build();
+		assertThat( options.getNamingStrategy().getSuiteName() ).isEqualTo( "name" );
+		verify( options.getFileNamerStrategy(), atLeastOnce() ).createFileNamer( "name" );
+	}
 
+	@ParameterizedTest
+	@MethodSource( "suiteNamesProjectLayout" )
+	void suiteName_should_always_overwrite_the_naming_strategy_of_project_layout(
+			final RecheckOptionsBuilder builder ) {
+		final RecheckOptions options = builder.build();
+		assertThat( options.getNamingStrategy().getSuiteName() ).isEqualTo( "name" );
+		verify( options.getProjectLayout(), atLeastOnce() ).getSuiteFolder( "name" );
+	}
+
+	static Stream<RecheckOptionsBuilder> suiteNamesFileNameStrategy() {
 		final FileNamer fileNamer = mock( FileNamer.class );
 		when( fileNamer.getFile( any() ) ).thenReturn( new File( "src/test/resources/retest/recheck/name" ) );
 
@@ -95,25 +112,31 @@ class RecheckOptionsTest {
 		when( fileNamerStrategy.createFileNamer( "name" ) ).thenReturn( fileNamer );
 
 		return Stream.of( //
-				RecheckOptions.builder() // name only
-						.suiteName( "name" ) //
-						.build(), //
-				RecheckOptions.builder() // name before naming
-						.suiteName( "name" ) //
-						.namingStrategy( namingStrategy ) //
-						.build(), //
-				RecheckOptions.builder() // name after naming
-						.namingStrategy( namingStrategy ) //
-						.suiteName( "name" ) //
-						.build(), //
 				RecheckOptions.builder() // name before file
 						.suiteName( "name" ) //
-						.fileNamerStrategy( fileNamerStrategy ) //
-						.build(), //
+						.fileNamerStrategy( fileNamerStrategy ), //
 				RecheckOptions.builder() // name after file
 						.fileNamerStrategy( fileNamerStrategy ) //
 						.suiteName( "name" ) //
-						.build() //
+		);
+	}
+
+	static Stream<RecheckOptionsBuilder> suiteNamesProjectLayout() {
+		final NamingStrategy namingStrategy = mock( NamingStrategy.class );
+		when( namingStrategy.getSuiteName() ).thenReturn( "suite" );
+		when( namingStrategy.getTestName() ).thenReturn( "test" );
+		final ProjectLayout projectLayout = mock( ProjectLayout.class );
+		when( projectLayout.getSuiteFolder( "name" ) ).thenReturn( new File( "name" ).toPath() );
+
+		return Stream.of( //
+				RecheckOptions.builder() // name before naming
+						.suiteName( "name" ) //
+						.namingStrategy( namingStrategy ) //
+						.projectLayout( projectLayout ), //
+				RecheckOptions.builder() // name after naming
+						.namingStrategy( namingStrategy ) //
+						.projectLayout( projectLayout )//
+						.suiteName( "name" ) //
 		);
 	}
 }
