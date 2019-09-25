64701ca8236090cfac476c9a185897ac5a70fd83
Daniel Kraus <daniel.kraus@retest.de>
2019-09-25 15:32:45 +0200

    Execute tests with Java 13

--- ci/script.sh
+++ ci/script.sh
@@ -9,7 +9,7 @@ set -o pipefail
 mvn ${MVN_ARGS} clean package -DskipTests
 
 # Test with JDK 11
-wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 11
+wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 13
 
 if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
     mvn ${MVN_ARGS} org.jacoco:jacoco-maven-plugin:prepare-agent test verify sonar:sonar
