832a9cc5adfccc1e876d9dc0a93460c25b5ca347
Pascal Allion <pascal.allion@retest.de>
2019-09-27 13:22:23 +0200

    Add more tests

--- src/test/java/de/retest/recheck/TestCaseFinderTest.java
+++ src/test/java/de/retest/recheck/TestCaseFinderTest.java
@@ -33,13 +33,57 @@ class TestCaseFinderTest {
 	}
 
 	@Nested
-	class SomeClass {
+	class SomeTestCase {
 
 		final Optional<String> className = TestCaseFinder.getInstance().findTestCaseClassNameInStack();
 
 		@Test
-		void test_case_finder_should_print_class_name() throws Exception {
-			assertThat( className ).isEmpty();
+		void test_case_finder_should_print_class_name_testcase() throws Exception {
+			assertThat( className ).hasValue( getClass().getName() );
+		}
+	}
+
+	@Nested
+	class SomeITCase {
+
+		final Optional<String> className = TestCaseFinder.getInstance().findTestCaseClassNameInStack();
+
+		@Test
+		void test_case_finder_should_print_class_name_itcase() throws Exception {
+			assertThat( className ).hasValue( getClass().getName() );
+		}
+	}
+
+	@Nested
+	class SomeTests {
+
+		final Optional<String> className = TestCaseFinder.getInstance().findTestCaseClassNameInStack();
+
+		@Test
+		void test_case_finder_should_print_class_name_tests() throws Exception {
+			assertThat( className ).hasValue( getClass().getName() );
+		}
+	}
+
+	@Nested
+	class TestSome {
+
+		final Optional<String> className = TestCaseFinder.getInstance().findTestCaseClassNameInStack();
+
+		@Test
+		void test_case_finder_should_print_class_name_prefix_test() throws Exception {
+			assertThat( className ).hasValue( getClass().getName() );
+		}
+	}
+
+	@Nested
+	class ITSome {
+
+		final Optional<String> className = TestCaseFinder.getInstance().findTestCaseClassNameInStack();
+
+		@Test
+		void test_case_finder_should_print_class_name_prefix_it() throws Exception {
+			assertThat( className ).hasValue( getClass().getName() );
 		}
 	}
 
