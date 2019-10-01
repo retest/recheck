b15a177c58db3389770406ec0fa6e071b07862e5
Johannes Dillmann <johannes.dillmann@retest.de>
2019-10-01 16:15:00 +0200

    Use test class(es) as report name

--- src/main/java/de/retest/recheck/persistence/CloudPersistence.java
+++ src/main/java/de/retest/recheck/persistence/CloudPersistence.java
@@ -6,11 +6,15 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import de.retest.recheck.Properties;
 import de.retest.recheck.auth.RetestAuthentication;
 import de.retest.recheck.persistence.bin.KryoPersistence;
 import de.retest.recheck.persistence.xml.XmlFolderPersistence;
+import de.retest.recheck.report.SuiteReplayResult;
+import de.retest.recheck.report.TestReport;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import lombok.extern.slf4j.Slf4j;
@@ -26,23 +30,44 @@ public class CloudPersistence<T extends Persistable> implements Persistence<T> {
 	@Override
 	public void save( final URI identifier, final T element ) throws IOException {
 		kryoPersistence.save( identifier, element );
-		if ( identifier.getPath().endsWith( Properties.AGGREGATED_TEST_REPORT_FILE_NAME ) ) {
-			saveToCloud( identifier );
+
+		if ( isAggregatedReport( identifier ) && element instanceof TestReport ) {
+			saveToCloud( identifier, (TestReport) element );
 		}
 	}
 
-	private void saveToCloud( final URI identifier ) throws IOException {
+	private boolean isAggregatedReport( final URI identifier ) {
+		return identifier.getPath().endsWith( Properties.AGGREGATED_TEST_REPORT_FILE_NAME );
+		}
+
+	private List<String> getTestClasses( final TestReport report ) {
+		return report.getSuiteReplayResults().stream() //
+				.map( SuiteReplayResult::getName ) //
+				.collect( Collectors.toList() );
+	}
+
+	private void saveToCloud( final URI identifier, final TestReport report ) throws IOException {
 		final HttpResponse<String> uploadUrlResponse = getUploadUrl();
 
+		final ReportUploadMetadata metadata = ReportUploadMetadata.builder() //
+				.location( identifier ) //
+				.uploadUrl( uploadUrlResponse.getBody() ) //
+				.testClasses( getTestClasses( report ) ) //
+				.build();
+
 		if ( uploadUrlResponse.isSuccess() ) {
-			uploadReport( identifier, uploadUrlResponse.getBody() );
+			uploadReport( metadata );
 		}
 	}
 
-	private void uploadReport( final URI identifier, final String url ) throws IOException {
-		final HttpResponse<?> uploadResponse = Unirest.put( url ) //
-				.header( "x-amz-meta-report-name", Paths.get( identifier ).getFileName().toString() )
-				.field( "upload", new File( identifier ) ) //
+	private void uploadReport( final ReportUploadMetadata metadata ) throws IOException {
+		final String reportName = metadata.getTestClasses() //
+				.stream() //
+				.collect( Collectors.joining( ", " ) );
+
+		final HttpResponse<?> uploadResponse = Unirest.put( metadata.getUploadUrl() ) //
+				.header( "x-amz-meta-report-name", reportName ) //
+				.field( "upload", new File( metadata.getLocation() ) ) //
 				.asEmpty();
 
 		if ( uploadResponse.isSuccess() ) {
