24ec256254c52446d793b9252b233ae453471eb7
Jeremias Roessler <roessler@retest.de>
2019-10-10 01:02:19 +0200

    A filter.js file should be loaded as JavaScript filter

--- src/main/java/de/retest/recheck/ignore/FilterLoader.java
+++ src/main/java/de/retest/recheck/ignore/FilterLoader.java
@@ -1,5 +1,7 @@
 package de.retest.recheck.ignore;
 
+import static de.retest.recheck.ignore.SearchFilterFiles.FILTER_JS_EXTENSION;
+
 import java.io.IOException;
 import java.nio.file.Path;
 
@@ -8,6 +10,9 @@ public interface FilterLoader {
 	Filter load() throws IOException;
 
 	static FilterLoader load( final Path path ) {
+		if ( path.toString().toLowerCase().endsWith( FILTER_JS_EXTENSION ) ) {
+			return () -> new JSFilterImpl( path );
+		}
 		return () -> Filters.load( path );
 	}
 
