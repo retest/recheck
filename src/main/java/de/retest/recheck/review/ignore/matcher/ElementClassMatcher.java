41f0ff7d691ab3a4db332b9c528f99a5500799e1
Daniel Kraus <daniel.kraus@retest.de>
2019-10-11 16:13:05 +0200

    Use list to guarantee order

--- src/main/java/de/retest/recheck/review/ignore/matcher/ElementClassMatcher.java
+++ src/main/java/de/retest/recheck/review/ignore/matcher/ElementClassMatcher.java
@@ -1,14 +1,11 @@
 package de.retest.recheck.review.ignore.matcher;
 
-import static java.util.stream.Collectors.toCollection;
-
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import de.retest.recheck.review.ignore.io.RegexLoader;
 import de.retest.recheck.ui.descriptors.Element;
@@ -17,19 +14,19 @@ public class ElementClassMatcher implements Matcher<Element> {
 
 	public static final String CLASS_KEY = "class";
 
-	private final Set<String> classValues;
+	private final List<String> classValues;
 
 	public ElementClassMatcher( final Element element ) {
-		this( toClassValuesSet( toClassValueString( element ) ) );
+		this( toClassValuesList( toClassValueString( element ) ) );
 	}
 
-	private ElementClassMatcher( final Set<String> classValues ) {
+	private ElementClassMatcher( final List<String> classValues ) {
 		this.classValues = classValues;
 	}
 
 	@Override
 	public boolean test( final Element element ) {
-		final Set<String> classValues = toClassValuesSet( toClassValueString( element ) );
+		final List<String> classValues = toClassValuesList( toClassValueString( element ) );
 		if ( classValues.isEmpty() ) {
 			return false;
 		}
@@ -56,7 +53,7 @@ public class ElementClassMatcher implements Matcher<Element> {
 		@Override
 		protected ElementClassMatcher load( final MatchResult matcher ) {
 			final String classValue = matcher.group( 1 );
-			return new ElementClassMatcher( toClassValuesSet( classValue ) );
+			return new ElementClassMatcher( toClassValuesList( classValue ) );
 		}
 	}
 
@@ -64,10 +61,10 @@ public class ElementClassMatcher implements Matcher<Element> {
 		return element.getIdentifyingAttributes().get( CLASS_KEY );
 	}
 
-	private static Set<String> toClassValuesSet( final String classValue ) {
+	private static List<String> toClassValuesList( final String classValue ) {
 		if ( classValue == null ) {
-			return Collections.emptySet();
+			return Collections.emptyList();
 		}
-		return Stream.of( classValue.split( " " ) ).collect( toCollection( HashSet::new ) );
+		return Arrays.asList( classValue.split( " " ) );
 	}
 }
