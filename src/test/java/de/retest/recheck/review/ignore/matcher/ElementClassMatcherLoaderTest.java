97c1138fba12c1e6f71776f434ecac8c9acbe834
Daniel Kraus <daniel.kraus@retest.de>
2019-10-11 10:55:27 +0200

    Add more (failing) tests for loader

--- src/test/java/de/retest/recheck/review/ignore/matcher/ElementClassMatcherLoaderTest.java
+++ src/test/java/de/retest/recheck/review/ignore/matcher/ElementClassMatcherLoaderTest.java
@@ -30,14 +30,38 @@ class ElementClassMatcherLoaderTest {
 	}
 
 	@Test
-	void save_should_produce_correct_line() {
+	void save_should_produce_correct_line_for_single_class_value() {
 		final String line = "class=some-class";
 		assertThat( cut.save( matcher ) ).isEqualTo( line );
 	}
 
 	@Test
-	void load_should_produce_correct_ignore() {
+	void load_should_produce_correct_ignore_for_single_class_value() {
 		final String line = "class=some-class";
 		assertThat( cut.save( cut.load( line ) ) ).isEqualTo( line );
 	}
+
+	@Test
+	void save_should_produce_correct_line_for_multiple_class_values() {
+		final String classValue = "one two";
+
+		final IdentifyingAttributes identifyingAttributes = mock( IdentifyingAttributes.class );
+		when( identifyingAttributes.get( ElementClassMatcher.CLASS_KEY ) ).thenReturn( classValue );
+
+		final Element element = mock( Element.class );
+		when( element.getIdentifyingAttributes() ).thenReturn( identifyingAttributes );
+
+		final ElementClassMatcher matcher = new ElementClassMatcher( element );
+
+		final String line = "class=" + classValue;
+
+		assertThat( cut.save( matcher ) ).isEqualTo( line );
+	}
+
+	@Test
+	void load_should_produce_correct_ignore_for_multiple_class_values() {
+		final String line = "class=one two";
+		assertThat( cut.save( cut.load( line ) ) ).isEqualTo( line );
+	}
+
 }
