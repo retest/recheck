eb580565896357c31c3681c09c5c7adb29f0adca
Daniel Kraus <daniel.kraus@retest.de>
2019-10-11 17:28:18 +0200

    Assert warning and error logs

--- src/test/java/de/retest/recheck/review/workers/LoadAndSaveFilterWorkerTest.java
+++ src/test/java/de/retest/recheck/review/workers/LoadAndSaveFilterWorkerTest.java
@@ -12,13 +12,24 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.io.TempDir;
 
 import de.retest.recheck.review.GlobalIgnoreApplier;
 import de.retest.recheck.review.counter.NopCounter;
+import de.retest.recheck.review.ignore.AttributeFilter.AttributeFilterLoader;
+import de.retest.recheck.review.ignore.FilterPreserveLineLoader;
+import de.retest.recheck.review.ignore.io.ErrorHandlingLoader;
 import de.retest.recheck.util.junit.jupiter.SystemProperty;
+import io.github.netmikey.logunit.api.LogCapturer;
 
 class LoadAndSaveFilterWorkerTest {
+
+	@RegisterExtension
+	LogCapturer warningAndErrorLogs = LogCapturer.create() //
+			.captureForType( AttributeFilterLoader.class ) //
+			.captureForType( FilterPreserveLineLoader.class ) //
+			.captureForType( ErrorHandlingLoader.class );
 
 	Path origIgnoreFile;
 	Path tempIgnoreFile;
@@ -44,8 +55,18 @@ class LoadAndSaveFilterWorkerTest {
 		final LoadFilterWorker load = new LoadFilterWorker( NopCounter.getInstance() );
 		final GlobalIgnoreApplier gia = load.load();
 		final SaveFilterWorker save = new SaveFilterWorker( gia );
+
 		save.save();
+
 		assertThat( tempIgnoreFile ).hasSameContentAs( origIgnoreFile );
+
+		assertThat( warningAndErrorLogs.size() ).isEqualTo( 3 );
+		warningAndErrorLogs.assertContains(
+				"'attribute=possible-*regex' contains '*'. For regular expressions, please use 'attribute-regex=possible-*regex'." );
+		warningAndErrorLogs.assertContains( "Please remove leading whitespace from the following line:\n" //
+				+ " match: foo" );
+		warningAndErrorLogs.assertContains(
+				"For ignoring an attribute globally, please use 'attribute=' (to ensure weired line breaks do not break your ignore file)." );
 	}
 
 }
