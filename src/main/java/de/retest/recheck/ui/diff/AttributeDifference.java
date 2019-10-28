1e0050e1c2683a0ff5eeb3e8de9b1d6e9f88bbad
Daniel Kraus <daniel.kraus@retest.de>
2019-10-28 11:15:11 +0100

    Refactor

--- src/main/java/de/retest/recheck/ui/diff/AttributeDifference.java
+++ src/main/java/de/retest/recheck/ui/diff/AttributeDifference.java
@@ -125,7 +125,7 @@ public class AttributeDifference implements LeafDifference, Comparable<Attribute
 			if ( isNullOrEmptyString( expected ) ) {
 				return;
 			}
-		} else if ( expected != null && fromAttribute.equals( expected ) ) {
+		} else if ( Objects.equals( fromAttribute, expected ) ) {
 			return;
 		}
 		logger.warn(
