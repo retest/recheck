5d945a1f26aac472497f4fad25a2bc6d46b35a67
Pascal Allion <pascal.allion@retest.de>
2019-10-28 16:50:10 +0100

    Add test for changes on deleted gm

--- src/test/java/de/retest/suite/flow/ApplyChangesToStatesFlowTest.java
+++ src/test/java/de/retest/suite/flow/ApplyChangesToStatesFlowTest.java
@@ -25,6 +25,7 @@ import de.retest.recheck.ui.diff.AttributeDifference;
 import de.retest.recheck.ui.review.ReviewResult;
 import de.retest.recheck.ui.review.ScreenshotChanges;
 import de.retest.recheck.ui.review.SuiteChangeSet;
+import de.retest.recheck.ui.review.TestChangeSet;
 
 public class ApplyChangesToStatesFlowTest {
 
@@ -50,8 +51,8 @@ public class ApplyChangesToStatesFlowTest {
 	public void apply_should_apply_changes_to_states() throws Exception {
 		final PersistenceFactory persistenceFactory =
 				new PersistenceFactory( new HashSet<>( Arrays.asList( StdXmlClassesProvider.getXmlDataClasses() ) ) );
-
 		final Persistence<SutState> persistence = persistenceFactory.getPersistence();
+
 		final File originalFile = new File( FILE );
 		final File temporaryFile = temp.newFile( "check_GUI_with_review_license.launch.recheck" );
 
@@ -69,6 +70,7 @@ public class ApplyChangesToStatesFlowTest {
 				.add( toChange.getIdentifyingAttributes(), new AttributeDifference( "enabled", "false", "true" ) );
 
 		ApplyChangesToStatesFlow.apply( persistence, acceptedChanges );
+
 		final SutState after = persistence.load( temporaryFile.toURI() );
 		final Element changed = after.getRootElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements()
 				.get( 0 ).getContainedElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements()
@@ -77,5 +79,67 @@ public class ApplyChangesToStatesFlowTest {
 		assertThat( changed.getAttributes().get( "enabled" ) ).isEqualTo( true );
 
 		// even more advanced: check if difference between before and after is acceptedChanges!
+	}
+
+	@Test
+	public void apply_should_not_fail_immediatly_when_a_gm_cannot_be_found() throws Exception {
+		final PersistenceFactory persistenceFactory =
+				new PersistenceFactory( new HashSet<>( Arrays.asList( StdXmlClassesProvider.getXmlDataClasses() ) ) );
+		final Persistence<SutState> persistence = persistenceFactory.getPersistence();
+
+		final File originalFile = new File( FILE );
+		final File temporaryFile1 = temp.newFile( "check_GUI_with_review_license.launch1.recheck" );
+		final File temporaryFile2 = temp.newFile( "check_GUI_with_review_license.launch2.recheck" );
+		final File temporaryFile3 = temp.newFile( "check_GUI_with_review_license.launch3.recheck" );
+
+		FileUtils.copyFile( originalFile, temporaryFile1 );
+		final SutState before1 = persistence.load( temporaryFile1.toURI() );
+
+		FileUtils.copyFile( originalFile, temporaryFile2 );
+		final SutState before2 = persistence.load( temporaryFile2.toURI() );
+
+		FileUtils.copyFile( originalFile, temporaryFile3 );
+		final SutState before3 = persistence.load( temporaryFile3.toURI() );
+
+		final SuiteChangeSet acceptedChanges =
+				buildChanges( temporaryFile1, temporaryFile2, temporaryFile3, before1, before2, before3 );
+
+		temporaryFile1.delete();
+		temporaryFile3.delete();
+
+		ApplyChangesToStatesFlow.apply( persistence, acceptedChanges );
+
+		final SutState after2 = persistence.load( temporaryFile2.toURI() );
+		final Element changed2 = after2.getRootElements().get( 0 ).getContainedElements().get( 0 )
+				.getContainedElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements().get( 0 )
+				.getContainedElements().get( 0 );
+
+		assertThat( changed2.getAttributes().get( "enabled" ) ).isEqualTo( true );
+	}
+
+	private SuiteChangeSet buildChanges( final File temporaryFile1, final File temporaryFile2,
+			final File temporaryFile3, final SutState before1, final SutState before2, final SutState before3 ) {
+		final ReviewResult reviewResult = new ReviewResult();
+		final SuiteChangeSet acceptedChanges = reviewResult.createSuiteChangeSet( SUITE, "" );
+		final Element toChange1 = before1.getRootElements().get( 0 ).getContainedElements().get( 0 )
+				.getContainedElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements().get( 0 )
+				.getContainedElements().get( 0 );
+		final Element toChange2 = before2.getRootElements().get( 0 ).getContainedElements().get( 0 )
+				.getContainedElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements().get( 0 )
+				.getContainedElements().get( 0 );
+		final Element toChange3 = before3.getRootElements().get( 0 ).getContainedElements().get( 0 )
+				.getContainedElements().get( 0 ).getContainedElements().get( 0 ).getContainedElements().get( 0 )
+				.getContainedElements().get( 0 );
+		final TestChangeSet testChangeSet = acceptedChanges.createTestChangeSet();
+		testChangeSet.createActionChangeSet( "launch1", temporaryFile1.getName(), ScreenshotChanges.empty() )
+				.getAttributesChanges()
+				.add( toChange1.getIdentifyingAttributes(), new AttributeDifference( "enabled", "false", "true" ) );
+		testChangeSet.createActionChangeSet( "launch2", temporaryFile2.getName(), ScreenshotChanges.empty() )
+				.getAttributesChanges()
+				.add( toChange2.getIdentifyingAttributes(), new AttributeDifference( "enabled", "false", "true" ) );
+		testChangeSet.createActionChangeSet( "launch3", temporaryFile3.getName(), ScreenshotChanges.empty() )
+				.getAttributesChanges()
+				.add( toChange3.getIdentifyingAttributes(), new AttributeDifference( "enabled", "false", "true" ) );
+		return acceptedChanges;
 	}
 }
