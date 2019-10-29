bb84623dd4b32036ba9a4d97754bf335a5ffcb8a
Daniel Kraus <daniel.kraus@retest.de>
2019-10-29 16:12:55 +0100

    Indent each missing file

--- src/main/java/de/retest/recheck/persistence/NoGoldenMasterFoundException.java
+++ src/main/java/de/retest/recheck/persistence/NoGoldenMasterFoundException.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NoGoldenMasterFoundException extends Exception {
 
@@ -12,9 +13,12 @@ public class NoGoldenMasterFoundException extends Exception {
 	private final List<String> filenames;
 
 	public NoGoldenMasterFoundException( final String... filenames ) {
-		super( "No Golden Master file(s) with the following name(s) found: \n"
-				+ Arrays.asList( filenames ).stream().collect( Collectors.joining( "\n" ) ) );
+		super( "No Golden Master file(s) with the following name(s) found:\n" + listFilenames( filenames ) );
 		this.filenames = Arrays.asList( filenames );
+	}
+
+	private static String listFilenames( final String[] filenames ) {
+		return Stream.of( filenames ).collect( Collectors.joining( "\n\t", "\t", "" ) );
 	}
 
 	public List<String> getFilenames() {
