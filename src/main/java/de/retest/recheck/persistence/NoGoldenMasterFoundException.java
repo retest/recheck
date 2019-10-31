291806c9c42a76b49f3ca84684f6e086d48811ca
diba1013 <bastian.diehl@retest.de>
2019-10-31 15:59:08 +0100

    Properly format NoGoldenMasterFoundException
    so that it is clearer what the actual problem is. Furthermore prevent passing no files to prevent improper formatting since Collectors.joining with a prefix still prints the prefix which results in ugly formatting (as this should not be allowed anyways).

--- src/main/java/de/retest/recheck/persistence/NoGoldenMasterFoundException.java
+++ src/main/java/de/retest/recheck/persistence/NoGoldenMasterFoundException.java
@@ -6,19 +6,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public class NoGoldenMasterFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+	private static final String GOLDEN_MASTER_PREFIX = "\n\t- ";
+
 	private final List<String> filenames;
 
 	public NoGoldenMasterFoundException( final String... filenames ) {
-		super( "No Golden Master file(s) with the following name(s) found:\n" + listFilenames( filenames ) );
+		super( "The following Golden Master(s) cannot be found:" + listFilenames( filenames ) );
 		this.filenames = Arrays.asList( filenames );
 	}
 
 	private static String listFilenames( final String[] filenames ) {
-		return Stream.of( filenames ).collect( Collectors.joining( "\n\t", "\t", "" ) );
+		if ( ArrayUtils.isEmpty( filenames ) ) {
+			throw new IllegalArgumentException( "You should at least provide one Golden Master file." );
+		}
+		return Stream.of( filenames ).collect( Collectors.joining( GOLDEN_MASTER_PREFIX, GOLDEN_MASTER_PREFIX, "" ) );
 	}
 
 	public List<String> getFilenames() {
--- src/test/java/de/retest/recheck/persistence/NoGoldenMasterFoundExceptionTest.java
+++ src/test/java/de/retest/recheck/persistence/NoGoldenMasterFoundExceptionTest.java
@@ -1,18 +1,34 @@
 package de.retest.recheck.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 class NoGoldenMasterFoundExceptionTest {
 
 	@Test
-	void should_format_filenames() throws Exception {
-		final NoGoldenMasterFoundException cut0 = new NoGoldenMasterFoundException( "foo" );
-		assertThat( cut0 ).hasMessageEndingWith( "\tfoo" );
+	void should_fail_if_no_files_provided() throws Exception {
+		assertThatThrownBy( NoGoldenMasterFoundException::new ) //
+				.isInstanceOf( IllegalArgumentException.class )
+				.hasMessage( "You should at least provide one Golden Master file." );
+	}
 
-		final NoGoldenMasterFoundException cut1 = new NoGoldenMasterFoundException( "foo", "bar" );
-		assertThat( cut1 ).hasMessageEndingWith( "\tfoo\n\tbar" );
+	@Test
+	void should_format_single_golden_master_accordingly() throws Exception {
+		final NoGoldenMasterFoundException cut = new NoGoldenMasterFoundException( "foo" );
+
+		assertThat( cut ).hasMessage( "The following Golden Master(s) cannot be found:\n" //
+				+ "\t- foo" );
+	}
+
+	@Test
+	void should_format_multiple_golden_masters_accordingly() throws Exception {
+		final NoGoldenMasterFoundException cut = new NoGoldenMasterFoundException( "foo", "bar" );
+
+		assertThat( cut ).hasMessage( "The following Golden Master(s) cannot be found:\n" //
+				+ "\t- foo\n" //
+				+ "\t- bar" );
 	}
 
 }
