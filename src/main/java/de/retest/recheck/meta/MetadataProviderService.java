0c695643109ab72ec592c686bc0d624a48e432cc
diba1013 <bastian.diehl@retest.de>
2019-12-03 15:25:59 +0100

    Properly implement MetadataProvider to make the code testable

--- src/main/java/de/retest/recheck/meta/MetadataProviderService.java
+++ src/main/java/de/retest/recheck/meta/MetadataProviderService.java
@@ -1,13 +1,26 @@
 package de.retest.recheck.meta;
 
+import java.util.Map;
+
 /**
  * Combines the {@link GlobalMetadataProvider} and an arbitrary local metadata provider.
  */
-public final class MetadataProviderService {
+public final class MetadataProviderService implements MetadataProvider {
 
 	private static final MetadataProvider GLOBAL_METADATA_PROVIDER = new GlobalMetadataProvider();
 
+	private final MetadataProvider provider;
+
+	MetadataProviderService( final MetadataProvider global, final MetadataProvider local ) {
+		provider = MultiMetadataProvider.of( global, local );
+	}
+
 	public static MetadataProvider of( final MetadataProvider localMetadataProvider ) {
-		return MultiMetadataProvider.of( GLOBAL_METADATA_PROVIDER, localMetadataProvider );
+		return new MetadataProviderService( GLOBAL_METADATA_PROVIDER, localMetadataProvider );
+	}
+
+	@Override
+	public Map<String, String> retrieve() {
+		return provider.retrieve();
 	}
 }
