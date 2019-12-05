d641223b0754a1d2c0310cae380ff7015846a9c4
diba1013 <bastian.diehl@retest.de>
2019-12-05 16:05:55 +0100

    Replace method with SystemUtils#getHostName

--- src/main/java/de/retest/recheck/meta/global/MachineMetadataProvider.java
+++ src/main/java/de/retest/recheck/meta/global/MachineMetadataProvider.java
@@ -3,14 +3,13 @@ package de.retest.recheck.meta.global;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.SystemUtils;
+
 import de.retest.recheck.meta.MetadataProvider;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public final class MachineMetadataProvider implements MetadataProvider {
-
-	private static final String WINDOWS_COMPUTER_NAME = "COMPUTERNAME";
-	private static final String UNIX_HOST_NAME = "HOSTNAME";
 
 	public static final String MACHINE_NAME = "machine.name";
 
@@ -18,24 +17,11 @@ public final class MachineMetadataProvider implements MetadataProvider {
 	public Map<String, String> retrieve() {
 		final Map<String, String> map = new HashMap<>();
 
-		final String machineName = retrieveMachineName();
-		if ( machineName != null ) {
-			map.put( MACHINE_NAME, machineName );
+		final String hostName = SystemUtils.getHostName();
+		if ( hostName != null ) {
+			map.put( MACHINE_NAME, hostName );
 		}
 
 		return map;
-	}
-
-	// Refer to https://stackoverflow.com/a/33112997 and https://stackoverflow.com/a/40702767
-	private String retrieveMachineName() {
-		final String computerName = System.getenv( WINDOWS_COMPUTER_NAME );
-		if ( computerName != null ) {
-			return computerName;
-		}
-		final String hostname = System.getenv( UNIX_HOST_NAME );
-		if ( hostname != null ) {
-			return hostname;
-		}
-		return null;
 	}
 }
