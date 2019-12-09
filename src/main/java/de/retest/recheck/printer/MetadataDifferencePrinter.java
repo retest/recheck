d252531b444168781b51162ea258b571f38ad748
diba1013 <bastian.diehl@retest.de>
2019-12-09 13:38:44 +0100

    Add a whitelist for the important metadata differences
    as potentially only a few metadata is really relevant for the user, i.e. that have an effect on the resulting golden master. This would then mean, however, that we would include all web specific keys here, but I would argue that this is nothing new in this repo.
    
    Otherwise we would have to integrate a flag or similar to disable the printing of the metadata, since we have some tests that use approvals to test the output produced by a recheck test. This would go as far as to make this flag

--- src/main/java/de/retest/recheck/printer/MetadataDifferencePrinter.java
+++ src/main/java/de/retest/recheck/printer/MetadataDifferencePrinter.java
@@ -1,13 +1,23 @@
 package de.retest.recheck.printer;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import de.retest.recheck.meta.global.OSMetadataProvider;
 import de.retest.recheck.ui.diff.meta.MetadataDifference;
 import de.retest.recheck.ui.diff.meta.MetadataElementDifference;
 
 public class MetadataDifferencePrinter implements Printer<MetadataDifference> {
 
 	private static final String KEY_EXPECTED_ACTUAL_FORMAT = "%s: expected=\"%s\", actual=\"%s\"";
+
+	// Only print a selected amount of differences that potentially can break a test
+	private static final Set<String> DIFFERENCES_TO_PRINT = new HashSet<>( Arrays.asList( //
+			OSMetadataProvider.OS_NAME, //
+			OSMetadataProvider.OS_VERSION //
+	) );
 
 	@Override
 	public String toString( final MetadataDifference difference, final String indent ) {
@@ -17,11 +27,16 @@ public class MetadataDifferencePrinter implements Printer<MetadataDifference> {
 
 	private String printDifferences( final MetadataDifference difference, final String indent ) {
 		return difference.getDifferences().stream() //
+				.filter( this::shouldPrint ) //
 				.map( this::print ) //
 				.collect( Collectors.joining( "\n" + indent, "\n" + indent, "" ) );
 	}
 
-	private <R> String print( final MetadataElementDifference difference ) {
+	private boolean shouldPrint( final MetadataElementDifference difference ) {
+		return DIFFERENCES_TO_PRINT.contains( difference.getKey() );
+	}
+
+	private String print( final MetadataElementDifference difference ) {
 		return String.format( KEY_EXPECTED_ACTUAL_FORMAT, difference.getKey(), difference.getExpected(),
 				difference.getActual() );
 	}
