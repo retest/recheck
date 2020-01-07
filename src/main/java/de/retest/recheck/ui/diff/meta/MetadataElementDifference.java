f8459e9f4256c199c70d58e4e9fe4861caff6fae
Pascal Allion <pascal.allion@retest.de>
2020-01-07 16:50:13 +0100

    Make MetadataElementDifference Serializable as well because MetadataDifference is

--- src/main/java/de/retest/recheck/ui/diff/meta/MetadataElementDifference.java
+++ src/main/java/de/retest/recheck/ui/diff/meta/MetadataElementDifference.java
@@ -1,9 +1,13 @@
 package de.retest.recheck.ui.diff.meta;
 
+import java.io.Serializable;
+
 import lombok.Value;
 
 @Value
-public class MetadataElementDifference {
+public class MetadataElementDifference implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private final String key;
 	private final String expected;
