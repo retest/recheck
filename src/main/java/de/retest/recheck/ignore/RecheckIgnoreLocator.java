805c76d22495fb5df2065b7c640287a9a2b7c278
Jeremias Roessler <roessler@retest.de>
2020-01-14 10:42:15 +0100

    Use @Slf4j instead

--- src/main/java/de/retest/recheck/ignore/RecheckIgnoreLocator.java
+++ src/main/java/de/retest/recheck/ignore/RecheckIgnoreLocator.java
@@ -10,17 +10,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import de.retest.recheck.configuration.ProjectConfigurationUtil;
 import de.retest.recheck.review.GlobalIgnoreApplier;
 import de.retest.recheck.review.counter.NopCounter;
 import de.retest.recheck.review.workers.LoadFilterWorker;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class RecheckIgnoreLocator {
-
-	private static final Logger logger = LoggerFactory.getLogger( RecheckIgnoreLocator.class );
 
 	private final String filename;
 
@@ -53,9 +50,9 @@ public class RecheckIgnoreLocator {
 		try {
 			return loadFilterWorker.load();
 		} catch ( final NoSuchFileException | FileNotFoundException e ) {
-			logger.debug( "Ignoring missing suite or user ignore file." );
+			log.debug( "Ignoring missing suite or user ignore file." );
 		} catch ( final Exception e ) {
-			logger.error( "Exception loading suite or user ignore file.", e );
+			log.error( "Exception loading suite or user ignore file.", e );
 		}
 		return GlobalIgnoreApplier.create( NopCounter.getInstance() );
 	}
