e49831640802c3a50bd0d600db2b7f3945e9a316
Daniel Kraus <daniel.kraus@retest.de>
2020-03-27 08:11:43 +0100

    Run tests with newest JDK 14

--- ci/script.sh
+++ ci/script.sh
@@ -8,8 +8,8 @@ set -o pipefail
 # Compile with JDK 8
 mvn ${MVN_ARGS} clean package -DskipTests
 
-# Test with JDK 13
-wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 13
+# Test with JDK 14
+wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 14
 
 if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
     mvn ${MVN_ARGS} verify sonar:sonar -Pcoverage
