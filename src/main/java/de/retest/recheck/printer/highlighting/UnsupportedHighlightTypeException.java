f9e50a38f93df6ee6bab917b5668e30491c24173
lemoncurry <agape.anna@gmail.com>
2020-03-30 09:15:14 +0200

    Add classes for highlighting
    This package is added to enable highlighting in recheck.cli.
    The printers for test reports are hard to read despite formatting
    improvements. This commit adds the capability to highlight certain
    keywords (which may be expanded at any time, as needed).
    Because of the design of the printers, it was necessary to create a
    global highlighter.
    The interface highlighter may be implemented to create a
    custom highlighter. By default, the DefaultHighlighter is used, which
    just returns the original string without any modifications.

---
+++ src/main/java/de/retest/recheck/printer/highlighting/UnsupportedHighlightTypeException.java
@@ -0,0 +1,8 @@
+package de.retest.recheck.printer.highlighting;
+
+public class UnsupportedHighlightTypeException extends RuntimeException {
+
+	public UnsupportedHighlightTypeException( final String message ) {
+		super( message );
+	}
+}
