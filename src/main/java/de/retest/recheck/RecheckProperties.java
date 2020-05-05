6055ae316940103c6447f090a026c504fe4e1ea7
diba1013 <bastian.diehl@retest.de>
2020-05-05 11:16:26 +0200

    Fix unecessary static final modifier in interfaces

--- src/main/java/de/retest/recheck/RecheckProperties.java
+++ src/main/java/de/retest/recheck/RecheckProperties.java
@@ -35,57 +35,57 @@ public interface RecheckProperties extends Reloadable {
 	 * Various constants.
 	 */
 
-	static final String PROPERTY_VALUE_SEPARATOR = ";";
-	static final String ZIP_FOLDER_SEPARATOR = "/";
+	String PROPERTY_VALUE_SEPARATOR = ";";
+	String ZIP_FOLDER_SEPARATOR = "/";
 
-	static final String SCREENSHOT_FOLDER_NAME = "screenshot";
-	static final String RECHECK_FOLDER_NAME = "recheck";
-	static final String DEFAULT_XML_FILE_NAME = "retest.xml";
+	String SCREENSHOT_FOLDER_NAME = "screenshot";
+	String RECHECK_FOLDER_NAME = "recheck";
+	String DEFAULT_XML_FILE_NAME = "retest.xml";
 
-	static final String RETEST_FOLDER_NAME = ".retest";
-	static final String RETEST_PROPERTIES_FILE_NAME = "retest.properties";
+	String RETEST_FOLDER_NAME = ".retest";
+	String RETEST_PROPERTIES_FILE_NAME = "retest.properties";
 
-	static final String GOLDEN_MASTER_FILE_EXTENSION = ".recheck";
-	static final String TEST_REPORT_FILE_EXTENSION = ".report";
-	static final String AGGREGATED_TEST_REPORT_FILE_NAME = "tests" + TEST_REPORT_FILE_EXTENSION;
+	String GOLDEN_MASTER_FILE_EXTENSION = ".recheck";
+	String TEST_REPORT_FILE_EXTENSION = ".report";
+	String AGGREGATED_TEST_REPORT_FILE_NAME = "tests" + TEST_REPORT_FILE_EXTENSION;
 
 	/*
 	 * Properties, their key constants and related functionality.
 	 */
 
-	static final String IGNORE_ATTRIBUTES_PROPERTY_KEY = "de.retest.recheck.ignore.attributes";
+	String IGNORE_ATTRIBUTES_PROPERTY_KEY = "de.retest.recheck.ignore.attributes";
 
 	@Key( IGNORE_ATTRIBUTES_PROPERTY_KEY )
 	@DefaultValue( "" )
 	@Separator( PROPERTY_VALUE_SEPARATOR )
 	List<String> ignoreAttributes();
 
-	static final String ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY = "de.retest.recheck.elementMatchThreshold";
+	String ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY = "de.retest.recheck.elementMatchThreshold";
 
 	@Key( ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY )
 	@DefaultValue( "0.3" )
 	double elementMatchThreshold();
 
-	static final String ROOT_ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY = "de.retest.recheck.rootElementMatchThreshold";
+	String ROOT_ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY = "de.retest.recheck.rootElementMatchThreshold";
 
 	@Key( ROOT_ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY )
 	@DefaultValue( "0.8" )
 	double rootElementMatchThreshold();
 
-	static final String ROOT_ELEMENT_CONTAINED_CHILDREN_MATCH_THRESHOLD_PROPERTY_KEY =
+	String ROOT_ELEMENT_CONTAINED_CHILDREN_MATCH_THRESHOLD_PROPERTY_KEY =
 			"de.retest.recheck.rootElementContainedChildrenMatchThreshold";
 
 	@Key( ROOT_ELEMENT_CONTAINED_CHILDREN_MATCH_THRESHOLD_PROPERTY_KEY )
 	@DefaultValue( "0.5" )
 	double rootElementContainedChildrenMatchThreshold();
 
-	static final String REHUB_REPORT_UPLOAD_ENABLED_PROPERTY_KEY = "de.retest.recheck.rehub.reportUploadEnabled";
+	String REHUB_REPORT_UPLOAD_ENABLED_PROPERTY_KEY = "de.retest.recheck.rehub.reportUploadEnabled";
 
 	@Key( REHUB_REPORT_UPLOAD_ENABLED_PROPERTY_KEY )
 	@DefaultValue( "false" )
 	boolean rehubReportUploadEnabled();
 
-	static final String FILE_OUTPUT_FORMAT_PROPERTY_KEY = "de.retest.output.Format";
+	String FILE_OUTPUT_FORMAT_PROPERTY_KEY = "de.retest.output.Format";
 
 	@Key( FILE_OUTPUT_FORMAT_PROPERTY_KEY )
 	FileOutputFormat fileOutputFormat();
